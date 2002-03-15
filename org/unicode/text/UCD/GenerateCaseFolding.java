2c1c8a1ca6a46b013f87bcf47024c51bcdce933f
macchiato-oss <mark@macchiato.com>
2002-03-15 01:57:01 +0000

    jitterbug 0: updated to reorganized java directories
    git-svn-id: https://unicode.org/repos/unicodetools/trunk@26 13e8329f-0b23-4da4-9fe8-d0f6fe080806

--- org/unicode/text/UCD/GenerateCaseFolding.java
+++ org/unicode/text/UCD/GenerateCaseFolding.java
@@ -5,8 +5,8 @@
 *******************************************************************************
 *
 * $Source: /home/cvsroot/unicodetools/org/unicode/text/UCD/GenerateCaseFolding.java,v $
-* $Date: 2002-03-15 00:34:46 $
-* $Revision: 1.5 $
+* $Date: 2002-03-15 01:57:01 $
+* $Revision: 1.6 $
 *
 *******************************************************************************
 */
@@ -15,7 +15,7 @@ package com.ibm.text.UCD;
 
 import java.util.*;
 import java.io.*;
-import com.ibm.text.UTF16;
+import com.ibm.icu.text.UTF16;
 
 import com.ibm.text.utility.*;
 
