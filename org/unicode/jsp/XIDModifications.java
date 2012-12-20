83e52882efedba6f38898531fabc604b636a27ae
markdavis <mark@macchiato.com>
2012-12-20 21:27:10 +0000

    Some basic xmod fixes
    git-svn-id: https://unicode.org/repos/unicodetools/trunk@422 13e8329f-0b23-4da4-9fe8-d0f6fe080806

--- org/unicode/jsp/XIDModifications.java
+++ org/unicode/jsp/XIDModifications.java
@@ -4,29 +4,29 @@ import com.ibm.icu.dev.util.UnicodeMap;
 import com.ibm.icu.text.UnicodeSet;
 
 public class XIDModifications {
-  static UnicodeSet allowed = new UnicodeSet("[:XID_Continue:]");
+  static UnicodeSet allowed = new UnicodeSet();
   static UnicodeMap<String> reasons = new UnicodeMap<String>();
 
   static class MyReader extends FileUtilities.SemiFileReader {
-    
+    // add other
+	//  # @missing: 0000..10FFFF; restricted ; not-chars
     @Override
     protected boolean handleLine(int start, int end, String[] items) {
       String type = items[1];
       if (type.equals("allowed")) {
         allowed.add(start, end);
-        reasons.putAll(start, end, items[2]);
       } else if (type.equals("restricted")) {
-        allowed.remove(start, end);
-        reasons.putAll(start, end, items[2]);
+//        allowed.remove(start, end);
       } else {
         throw new IllegalArgumentException(type);
       }
+      reasons.putAll(start, end, items[2]);
       return true;
     }
   }
   static {
-    reasons.putAll(0,0x10FFFF,"ok");
-    reasons.putAll(new UnicodeSet("[[:gc=cn:][:gc=co:][:gc=cs:][:gc=cc:]-[:whitespace:]]"),"not-char");
+    reasons.putAll(0,0x10FFFF,"not-chars");
+    //reasons.putAll(new UnicodeSet("[[:gc=cn:][:gc=co:][:gc=cs:][:gc=cc:]-[:whitespace:]]"),"not-char");
     new MyReader().process(XIDModifications.class, "xidmodifications.txt");
     allowed.freeze();
     reasons.freeze();
