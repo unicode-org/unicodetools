a57f6017ae70ad28624d580433f8309fde17c527
Markus Scherer <markus.icu@gmail.com>
2013-07-18 19:46:59 +0000

    remove development branch for UCA 6.3; was merged into trunk at r490
    git-svn-id: https://unicode.org/repos/unicodetools/branches/markus@532 13e8329f-0b23-4da4-9fe8-d0f6fe080806

--- uca63/org/unicode/draft/TestTranslit.java
+++
@@ -1,7 +0,0 @@
-package org.unicode.draft;
-
-public class TestTranslit {
-public static void main(String[] args) {
-  
-}
-}
--- uca63/org/unicode/props/XMPProperties.java
+++
@@ -1,5 +0,0 @@
-package org.unicode.props;
-
-public class XMPProperties {
-
-}
