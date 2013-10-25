1b542d78ce177901fea44ab57eba1543c08c12ef
markdavis <mark@macchiato.com>
2013-10-25 13:45:57 +0000

    test idmod
    git-svn-id: https://unicode.org/repos/unicodetools/trunk@561 13e8329f-0b23-4da4-9fe8-d0f6fe080806

--- org/unicode/props/TestProperties.java
+++ org/unicode/props/TestProperties.java
@@ -1,25 +1,37 @@
 package org.unicode.props;
 
+import java.lang.reflect.Method;
+import java.util.EnumSet;
 import java.util.LinkedHashSet;
 import java.util.Map.Entry;
 import java.util.Set;
 
+import org.unicode.cldr.draft.ScriptCategories;
+import org.unicode.cldr.draft.ScriptMetadata;
+import org.unicode.cldr.draft.ScriptMetadata.IdUsage;
+import org.unicode.cldr.draft.ScriptMetadata.Info;
 import org.unicode.cldr.unittest.TestAll.TestInfo;
 import org.unicode.cldr.util.CLDRFile;
 import org.unicode.cldr.util.CLDRFile.DraftStatus;
 import org.unicode.cldr.util.CLDRFile.WinningChoice;
 import org.unicode.cldr.util.Factory;
 import org.unicode.cldr.util.LanguageTagParser;
+import org.unicode.draft.GetNames;
+import org.unicode.jsp.ScriptTester.ScriptExtensions;
+import org.unicode.props.PropertyNames.NameMatcher;
 import org.unicode.props.UcdPropertyValues.Age_Values;
 import org.unicode.props.UcdPropertyValues.General_Category_Values;
 import org.unicode.props.UcdPropertyValues.Numeric_Type_Values;
 
 import com.ibm.icu.dev.test.TestFmwk;
 import com.ibm.icu.dev.util.UnicodeMap;
+import com.ibm.icu.dev.util.UnicodeProperty;
 import com.ibm.icu.impl.Utility;
 import com.ibm.icu.lang.UCharacter;
 import com.ibm.icu.text.Normalizer2;
+import com.ibm.icu.text.UTF16;
 import com.ibm.icu.text.UnicodeSet;
+import com.ibm.icu.text.UnicodeSetIterator;
 import com.ibm.icu.util.ULocale;
 
 public class TestProperties extends TestFmwk {
@@ -43,10 +55,113 @@ public class TestProperties extends TestFmwk {
         show(UcdProperty.Confusable_MA);
     }
 
+    public void TestBuildingIdModType() {
+        UnicodeMap<String> simulateType = new UnicodeMap();
+        // we load the items in reverse order, so that we override with more "powerful" categories
+        //UnicodeMap<String> Script_Extensions = iup.load(UcdProperty.Script_Extensions);
+        // now simulate
+        //ScriptCategories sc = new ScriptCategories();
+
+        // start big, and whittle down
+        simulateType.putAll(0,0x10FFFF, "recommended");
+
+        // Script Metadata
+
+        // we do it this way so that if any of the scripts for a character are recommended, it is recommended.
+        ScriptMetadata smd = new ScriptMetadata();
+
+        //        UnicodeMap<String> Script = iup.load(UcdProperty.Script);
+        //        for (String script : Script.values()) {
+        //            UnicodeSet chars = Script.getSet(script);
+        //            Info info = ScriptMetadata.getInfo(script);
+        //            switch (info.idUsage) {
+        //            case LIMITED_USE:
+        //            case ASPIRATIONAL:
+        //                simulateType.putAll(chars, "limited_use");
+        //                break;
+        //            case EXCLUSION:
+        //                simulateType.putAll(chars, "historic");
+        //                break;
+        //            }
+        //        }
+        // this works because items with real values below will be Common above, and have no effect
+        UnicodeMap<String> Script_Extensions = iup.load(UcdProperty.Script_Extensions);
+        for (String value : Script_Extensions.values()) {
+            IdUsage bestIdUsage = getBestIdUsage(value);
+            UnicodeSet chars = Script_Extensions.getSet(value);
+            switch (bestIdUsage) {
+            case LIMITED_USE:
+            case ASPIRATIONAL:
+                simulateType.putAll(chars, "limited-use");
+                break;
+            case EXCLUSION:
+                simulateType.putAll(chars, "historic");
+                break;
+            }
+        }
+
+        simulateType.putAll(iup.load(UcdProperty.Deprecated).getSet("Yes"), "obsolete");
+
+        simulateType.putAll(iup.load(UcdProperty.XID_Continue).getSet("No"), "not-xid");
+
+        simulateType.putAll(iup.load(UcdProperty.NFKC_Quick_Check).getSet("No"), "not-NFKC");
+
+        UnicodeMap<String> General_Category = iup.load(UcdProperty.General_Category);
+        UnicodeSet White_Space = iup.load(UcdProperty.White_Space).getSet("Yes");
+        for (General_Category_Values gc : EnumSet.of(
+                General_Category_Values.Unassigned,
+                General_Category_Values.Private_Use,
+                General_Category_Values.Surrogate,
+                General_Category_Values.Control
+                )) {
+            UnicodeSet set = General_Category.getSet(gc.toString());
+            set = new UnicodeSet(set).removeAll(White_Space);
+            simulateType.putAll(set, "not-chars");
+        }
+
+        simulateType.putAll(iup.load(UcdProperty.Default_Ignorable_Code_Point).getSet("Yes"), "default-ignorable");
+
+        UnicodeMap<String> typeMap = iup.load(UcdProperty.Id_Mod_Type);
+        for (String type : typeMap.values()) {
+            UnicodeSet idmodSet = typeMap.getSet(type);
+            UnicodeSet simSet = simulateType.getSet(type);
+            UnicodeSet missing = new UnicodeSet(idmodSet).removeAll(simSet);
+            UnicodeSet same = new UnicodeSet(idmodSet).retainAll(simSet);
+            logln(type + "\tfound:\t" + same.size() + "\tmissing:\t" + missing.size() + "\t" + missing.toPattern(false));
+//            for (String type2 : simulateType.values()) {
+//                UnicodeSet simSet2 = simulateType.getSet(type2);
+//                UnicodeSet missing2 = new UnicodeSet(simSet2).retainAll(missing);
+//                if (missing2.size() != 0) {
+//                    logln("\tidmod:\t" + type + "\tsimulated:\t" + type2 + "\t" + missing2.size() + "\t" + missing2.toPattern(false));
+//                    for (UnicodeSetIterator it = new UnicodeSetIterator(missing2); it.nextRange();) {
+//                        logln("\t" + getCodeAndName(UTF16.valueOf(it.codepoint)));
+//                        if (it.codepointEnd != it.codepoint) {
+//                            logln("\t..." + getCodeAndName(UTF16.valueOf(it.codepointEnd)));
+//                        }
+//                    }
+//                }
+//            }
+        }
+    }
+
+    public IdUsage getBestIdUsage(String value) {
+        String[] scripts = value.split(" ");
+        IdUsage bestIdUsage = IdUsage.UNKNOWN;
+        for (String script : scripts) {
+            Info info = ScriptMetadata.getInfo(script);
+            IdUsage idUsage = info.idUsage;
+            if (bestIdUsage.compareTo(idUsage) < 0) {
+                bestIdUsage = idUsage;
+            }
+        }
+        return bestIdUsage;
+    }
+
+    static final  UnicodeMap<String> nameMap = iup.load(UcdProperty.Name);
+
     public void TestExemplarsAgainstIdmod() {
         UnicodeMap<String> statusMap = iup.load(UcdProperty.Id_Mod_Status);
         UnicodeMap<String> typeMap = iup.load(UcdProperty.Id_Mod_Type);
-        UnicodeMap<String> nameMap = iup.load(UcdProperty.Name);
         UnicodeMap<String> xidContinue = iup.load(UcdProperty.XID_Continue);
 
         Normalizer2 nfd = Normalizer2.getNFDInstance();
@@ -111,7 +226,7 @@ public class TestProperties extends TestFmwk {
             if (!suspicious.isEmpty()) {
                 logln(localeName + "\tSuspicious characters; never in CLDR data: ");
                 for (String cp : suspicious) {
-                    logln("\t" + Utility.hex(cp) + " (" + cp + ") " + nameMap.get(cp));
+                    logln("\t" + getCodeAndName(cp));
                 }
             }
         }
@@ -121,6 +236,10 @@ public class TestProperties extends TestFmwk {
         for (String locale :  nonapprovedLocales) {
             logln("No approved exemplars for " + english.getName(locale) + "\t" + locale);
         }
+    }
+
+    public String getCodeAndName(String cp) {
+        return Utility.hex(cp) + " (" + cp + ") " + nameMap.get(cp);
     }
 
     private UnicodeMap<String> show(UcdProperty ucdProperty) {
