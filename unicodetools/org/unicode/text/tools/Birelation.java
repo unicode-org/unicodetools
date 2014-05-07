6ad165094e56dafbf74e8732d04266ccd2cfc471
markdavis <mark@macchiato.com>
2014-05-07 14:20:21 +0000

    git-svn-id: https://unicode.org/repos/unicodetools/trunk@647 13e8329f-0b23-4da4-9fe8-d0f6fe080806

---
+++ unicodetools/org/unicode/text/tools/Birelation.java
@@ -0,0 +1,90 @@
+package org.unicode.text.tools;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
+
+import com.ibm.icu.dev.util.Relation;
+
+class Birelation<K,V> {
+    final Relation<K,V> keyToValues;
+    final Relation<V,K> valueToKeys;
+
+    Birelation(Map<K, Set<V>> map1, 
+            Map<V, Set<K>> map2, 
+            Class valueSetCreator, 
+            Class keySetCreator, 
+            Comparator<V> valueSetComparator,
+            Comparator<K> keySetComparator
+            ) {
+        keyToValues = Relation.of(map1, valueSetCreator, valueSetComparator);
+        valueToKeys = Relation.of(map2, keySetCreator, keySetComparator);
+    }
+    public static <K,V> Birelation<K, V> of(
+            Map<K, Set<V>> map1, 
+            Map<V, Set<K>> map2, 
+            Class setCreator1, 
+            Class setCreator2, 
+            Comparator<V> setComparator1,
+            Comparator<V> setComparator2
+            ) {
+        return new Birelation(map1, map2, setCreator1, setCreator2, setComparator1, setComparator2);
+    }
+    Birelation<K, V> add(K key, V value) {
+        keyToValues.put(key, value);
+        valueToKeys.put(value, key);
+        return this;
+    }
+
+    Birelation<K, V> remove(K key, V value) {
+        keyToValues.remove(key, value);
+        valueToKeys.remove(value, key);
+        return this;
+    }
+
+    public Birelation<K, V> removeKey(K key) {
+        Set<V> values = keyToValues.get(key);
+        if (values == null) {
+            return this;
+        }
+        for (V value : values) {
+            valueToKeys.remove(value, key);
+        }
+        keyToValues.removeAll(key);
+        return this;
+    }
+    public Set<V> getValues(K key) {
+        return keyToValues.get(key);
+    }
+    public Set<K> getKeys(V value) {
+        return valueToKeys.get(value);
+    }
+    public Set<Entry<K, Set<V>>> keyValuesSet() {
+        return keyToValues.keyValuesSet();
+    }
+    public Set<Entry<V, Set<K>>> valueKeysSet() {
+        return valueToKeys.keyValuesSet();
+    }
+    public void freeze() {
+        keyToValues.freeze();
+        valueToKeys.freeze();
+    }
+    public Set<K> keySet() {
+        return keyToValues.keySet();
+    }
+    public Set<V> valuesSet() {
+        return valueToKeys.keySet();
+    }
+    public Relation<Set<V>,K> getValuesToKeys() {
+        Relation<Set<V>,K> result = Relation.of(new HashMap(), LinkedHashSet.class);
+        for (Entry<K, Set<V>> entry : keyToValues.keyValuesSet()) {
+            K key = entry.getKey();
+            Set<V> values = entry.getValue();
+            result.put(values, key);
+        }
+        return result;
+    }
+}
---
+++ unicodetools/org/unicode/text/tools/OldEmojiProcessing.java
@@ -0,0 +1,96 @@
+package org.unicode.text.tools;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.Map.Entry;
+
+import org.unicode.text.tools.GenerateEmoji.Label;
+import org.unicode.text.utility.Utility;
+
+import com.ibm.icu.dev.util.CollectionUtilities;
+import com.ibm.icu.dev.util.UnicodeMap;
+import com.ibm.icu.impl.Row;
+
+public class OldEmojiProcessing {
+
+    static final Set<String> ANDROID_IMAGES = new TreeSet<>();
+    static final Map<Row.R2<Integer, Integer>,Integer> ANDROID_REMAP = new HashMap<>();
+    static final UnicodeMap<String> ANDROID_REMAP_VALUES = new UnicodeMap();
+    static {
+        addAndroidRemap("🇨🇳", 0xFE4ED); // cn
+        addAndroidRemap("🇩🇪", 0xFE4E8); // de
+        addAndroidRemap("🇪🇸", 0xFE4ED); // es
+        addAndroidRemap("🇫🇷", 0xFE4E7); // fr
+        addAndroidRemap("🇬🇧", 0xfe4eA); // gb
+        addAndroidRemap("🇮🇹", 0xFE4E9); // it
+        addAndroidRemap("🇯🇵", 0xFE4E5); // ja
+        addAndroidRemap("🇰🇷", 0xFE4EE); // ko
+        addAndroidRemap("🇷🇺", 0xFE4EC); // ru
+        addAndroidRemap("🇺🇸", 0xFE4E6); // us
+        addAndroidRemap("#⃣", 0xFE82C);
+        for (int i = 1; i <= 9; ++i) {
+            addAndroidRemap((char)('0' + i) + "" + Emoji.ENCLOSING_KEYCAP, 0xFE82D + i); // 1 => U+FE82E
+        }
+        addAndroidRemap("0⃣", 0xFE837);
+    }
+
+    public static Integer addAndroidRemap(String real, int replacement) {
+        ANDROID_REMAP_VALUES.put(replacement, real);
+        int first = real.codePointAt(0);
+        return ANDROID_REMAP.put(Row.of(first, real.codePointAt(Character.charCount(first))), replacement);
+    }
+    
+    public static String androidPng(int firstCodepoint, int secondCodepoint, boolean first) {
+        if (secondCodepoint == Emoji.ENCLOSING_KEYCAP) {
+            int debug = 0;
+        }
+        if (secondCodepoint != 0) {
+            Integer remapped = ANDROID_REMAP.get(Row.of(firstCodepoint, secondCodepoint));
+            if (remapped != null) {
+                if (!first) {
+                    return null;
+                }
+                firstCodepoint = remapped;
+            } else {
+                return null;
+            }
+        }
+        String filename = "android/emoji_u" + Utility.hex(first ? firstCodepoint : secondCodepoint).toLowerCase(Locale.ENGLISH) + ".png";
+        ANDROID_IMAGES.add(filename);
+        return filename;
+    }
+    //Collator.getInstance(ULocale.ENGLISH);
+
+    //    static final Map<String,String> REMAP_FLAGS = new HashMap();
+    //    static {
+    //        addFlagRemap("BL", "FR");
+    //        addFlagRemap("BV", "NO");
+    //        addFlagRemap("GF", "FR");
+    //        addFlagRemap("HM", "AU");   
+    //        addFlagRemap("MF", "FR");
+    //        addFlagRemap("RE", "FR");
+    //        addFlagRemap("SJ", "NO");
+    //        addFlagRemap("TF", "FR");
+    //        addFlagRemap("UM", "US");
+    //        addFlagRemap("WF", "FR");
+    //        addFlagRemap("YT", "FR");
+    //    }
+    //    public static void addFlagRemap(String originalCountry, String replacementCountry) {
+    //        REMAP_FLAGS.put(originalCountry, replacementCountry);
+    //        System.out.println(
+    //                Emoji.buildFileName(Emoji.getHexFromFlagCode(originalCountry),"_") 
+    //                + " => "
+    //                + Emoji.buildFileName(Emoji.getHexFromFlagCode(replacementCountry),"_"));
+    //    }
+
+    
+
+}
