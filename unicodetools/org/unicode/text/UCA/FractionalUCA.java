11782efb648ce2f0f751259357a8c036db9f3869
Markus Scherer <markus.icu@gmail.com>
2014-07-21 02:06:43 +0000

    FractionalUCA: print Han ideographs in radical-stroke order with ranges
    git-svn-id: https://unicode.org/repos/unicodetools/trunk@695 13e8329f-0b23-4da4-9fe8-d0f6fe080806

--- unicodetools/org/unicode/text/UCA/FractionalUCA.java
+++ unicodetools/org/unicode/text/UCA/FractionalUCA.java
@@ -580,7 +580,8 @@ public class FractionalUCA {
         printUnifiedIdeographRanges(fractionalLog);
         fractionalLog.println();
         RadicalStroke radicalStroke = new RadicalStroke(getCollator().getUCDVersion());
-        for (int pos = 0; pos >= 0; pos = radicalStroke.printNextRadical(pos)) {}
+        radicalStroke.printRadicalStrokeOrder(fractionalLog);
+        fractionalLog.println();
 
         // Print the [top_byte] information before any of the mappings
         // so that parsers can use this data while working with the fractional primary weights,
