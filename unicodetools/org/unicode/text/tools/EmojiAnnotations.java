df8a4c3372159b3cac76aa96818127a452d66f7b
Mark Davis <mark@macchiato.com>
2015-02-25 11:28:18 +0000

    separated out annotations to make them easier to manage; suppressed groups in .xml
    git-svn-id: https://unicode.org/repos/unicodetools/trunk@782 13e8329f-0b23-4da4-9fe8-d0f6fe080806

--- unicodetools/org/unicode/text/tools/EmojiAnnotations.java
+++ unicodetools/org/unicode/text/tools/EmojiAnnotations.java
@@ -22,7 +22,7 @@ public class EmojiAnnotations extends Birelation<String,String> {
             "states", "sar", "people's", "minor",
             "sts."));
 
-    public EmojiAnnotations(Comparator codepointCompare, String filename) {
+    public EmojiAnnotations(Comparator codepointCompare, String... filenames) {
         super(new TreeMap(codepointCompare), 
                 new TreeMap(codepointCompare), 
                 TreeSet.class, 
@@ -31,31 +31,36 @@ public class EmojiAnnotations extends Birelation<String,String> {
                 codepointCompare);
 
         Output<Set<String>> lastLabel = new Output(new TreeSet<String>(codepointCompare));
-        for (String line : FileUtilities.in(EmojiAnnotations.class, filename)) {
-            line = line.trim();
-            if (line.isEmpty() || line.startsWith("#")) {
-                continue;
-            }
-            if (line.contains("closed")) {
-                int debug = 0;
-            }
-            line = Emoji.getLabelFromLine(lastLabel, line);
-            for (int i = 0; i < line.length();) {
-                line = Emoji.UNESCAPE.transform(line);
-                String string = Emoji.getEmojiSequence(line, i);
-                if (Emoji.ASCII_LETTERS.containsSome(string)) {
-                    UnicodeSet overlap = new UnicodeSet().addAll(string).retainAll(Emoji.ASCII_LETTERS);
-                    String withPosition = line.replaceAll("("+overlap+")", "###$1");
-                    throw new IllegalArgumentException("Strange line with ASCII emoji: " + overlap + "; "+ withPosition);
-                }
-                i += string.length();
-                if (Emoji.skipEmojiSequence(string)) {
+        for (String filename : filenames) {
+            int lineCount = 0;
+            for (String line : FileUtilities.in(EmojiAnnotations.class, filename)) {
+                line = line.trim();
+                if (line.isEmpty() || line.startsWith("#")) {
                     continue;
                 }
-                for (String item : lastLabel.value) {
-                    add(item.toLowerCase(Locale.ENGLISH), string);
+                if (line.contains("closed")) {
+                    int debug = 0;
+                }
+                lineCount++;
+                line = Emoji.getLabelFromLine(lastLabel, line);
+                for (int i = 0; i < line.length();) {
+                    line = Emoji.UNESCAPE.transform(line);
+                    String string = Emoji.getEmojiSequence(line, i);
+                    if (Emoji.ASCII_LETTERS.containsSome(string)) {
+                        UnicodeSet overlap = new UnicodeSet().addAll(string).retainAll(Emoji.ASCII_LETTERS);
+                        String withPosition = line.replaceAll("("+overlap+")", "###$1");
+                        throw new IllegalArgumentException("Strange line with ASCII emoji: " + overlap + "; "+ withPosition);
+                    }
+                    i += string.length();
+                    if (Emoji.skipEmojiSequence(string)) {
+                        continue;
+                    }
+                    for (String item : lastLabel.value) {
+                        add(item.toLowerCase(Locale.ENGLISH), string);
+                    }
                 }
             }
+            System.out.println(lineCount + "\tannotation lines from " + filename);
         }
         UnicodeSet temp = new UnicodeSet(Emoji.APPLE)
         .removeAll(getValues("people-apple"))
@@ -79,6 +84,19 @@ public class EmojiAnnotations extends Birelation<String,String> {
             add("other-android", s);
         }
         
+        temp = new UnicodeSet(Emoji.EMOJI_CHARS)
+        .removeAll(getValues("people"))
+        .removeAll(getValues("nature"))
+        .removeAll(getValues("objects"))
+        .removeAll(getValues("places"))
+        .removeAll(getValues("symbols"))
+        .removeAll(getValues("flag"));
+        System.out.println("other: " + temp.toPattern(false));
+        for (String s : temp) {
+            add("other", s);
+        }
+
+
         //        for (String s : FITZ_MINIMAL) {
         //            ANNOTATIONS_TO_CHARS.add("fitz-minimal", s);
         //        }
@@ -92,29 +110,29 @@ public class EmojiAnnotations extends Birelation<String,String> {
         //                ANNOTATIONS_TO_CHARS.add("mark", s);
         //            }
         //        }
-        for (int cp1 = Emoji.FIRST_REGIONAL; cp1 <= Emoji.LAST_REGIONAL; ++cp1) {
-            for (int cp2 = Emoji.FIRST_REGIONAL; cp2 <= Emoji.LAST_REGIONAL; ++cp2) {
-                String emoji = new StringBuilder().appendCodePoint(cp1).appendCodePoint(cp2).toString();
-                if (Emoji.EMOJI_CHARS.contains(emoji)) {
-                    add("flag", emoji);
-                }
-                //String regionCode = GenerateEmoji.getFlagCode(emoji);
-            }
-        }
-        // get extra names
-//        for (String name : CountryCodeConverter.names()) {
-//            String regionCode = CountryCodeConverter.getCodeFromName(name);
-//            if (regionCode == null || regionCode.length() != 2) {
-//                continue;
+//        for (int cp1 = Emoji.FIRST_REGIONAL; cp1 <= Emoji.LAST_REGIONAL; ++cp1) {
+//            for (int cp2 = Emoji.FIRST_REGIONAL; cp2 <= Emoji.LAST_REGIONAL; ++cp2) {
+//                String emoji = new StringBuilder().appendCodePoint(cp1).appendCodePoint(cp2).toString();
+//                if (Emoji.EMOJI_CHARS.contains(emoji)) {
+//                    add("flag", emoji);
+//                }
+//                //String regionCode = GenerateEmoji.getFlagCode(emoji);
 //            }
-//            if (regionCode.equals("RS") 
-//                    && name.contains("montenegro")) {
-//                continue;
-//            }
-//            String emoji = Emoji.getEmojiFromRegionCode(regionCode);
-//            //System.out.println(regionCode + "=>" + name);
-//            addParts(emoji, name);
 //        }
+        // get extra names
+        //        for (String name : CountryCodeConverter.names()) {
+        //            String regionCode = CountryCodeConverter.getCodeFromName(name);
+        //            if (regionCode == null || regionCode.length() != 2) {
+        //                continue;
+        //            }
+        //            if (regionCode.equals("RS") 
+        //                    && name.contains("montenegro")) {
+        //                continue;
+        //            }
+        //            String emoji = Emoji.getEmojiFromRegionCode(regionCode);
+        //            //System.out.println(regionCode + "=>" + name);
+        //            addParts(emoji, name);
+        //        }
         freeze();
         UnicodeSet annotationCharacters = new UnicodeSet().addAll(valuesSet());
         if (!annotationCharacters.containsAll(Emoji.EMOJI_CHARS)) {
@@ -123,16 +141,16 @@ public class EmojiAnnotations extends Birelation<String,String> {
         }
     }
 
-    public void addParts(String emoji, String name) {
-        name = name.toLowerCase(Locale.ENGLISH);
-        for (String namePart : name.split("[- ,&\\(\\)]+")) {
-            if (STOP_WORDS.contains(namePart)) {
-                continue;
-            }
-            if (namePart.startsWith("d’") || namePart.startsWith("d'")) {
-                namePart = namePart.substring(2);
-            }
-            add(namePart, emoji);
-        }
-    }
+//    public void addParts(String emoji, String name) {
+//        name = name.toLowerCase(Locale.ENGLISH);
+//        for (String namePart : name.split("[- ,&\\(\\)]+")) {
+//            if (STOP_WORDS.contains(namePart)) {
+//                continue;
+//            }
+//            if (namePart.startsWith("d’") || namePart.startsWith("d'")) {
+//                namePart = namePart.substring(2);
+//            }
+//            add(namePart, emoji);
+//        }
+//    }
 }
---
+++ unicodetools/org/unicode/text/tools/MakeEmojiTable.java
@@ -0,0 +1,56 @@
+package org.unicode.text.tools;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Locale;
+
+import org.unicode.cldr.draft.FileUtilities;
+
+import com.ibm.icu.dev.util.BagFormatter;
+import com.ibm.icu.impl.Utility;
+import com.ibm.icu.text.UTF16;
+
+public class MakeEmojiTable {
+    public static void main(String[] args) throws IOException {
+        PrintWriter out = BagFormatter.openUTF8Writer(Emoji.TR51_OUTPUT_DIR, "emoji-glyphs.html");
+        GenerateEmoji.writeHeader(out, "recommended glyphs", "");
+        boolean first = true;
+        boolean firstText = true;
+        out.println("<tr><th>Code</th><th>Ref</th><th>Apple</th><th>Andr.</th><th>Name</th><th>Remarks</th></tr>");
+        for (String line : FileUtilities.in(MakeEmojiTable.class, "emojiGlyphs.txt")) {
+            line = line.trim();
+            if (line.isEmpty()) {
+                continue;
+            } else if (line.startsWith("U+")) {
+                int hex = Integer.parseInt(line.substring(2), 16);
+                if (first) {
+                    first = false;
+                    out.println("</td></tr>");
+                }
+                String chars = UTF16.valueOf(hex);
+                String hexUpper = Utility.hex(hex);
+                String hexLower = hexUpper.toLowerCase(Locale.ROOT);
+                out.println("<tr>"
+                        + "<td>" + hexUpper + "</td>"
+                        + "<td><img class='imgb' alt='" + chars + "' src='images/ref/ref_" + hexLower + ".png'></td>"
+                        + "<td><img class='imgb' alt='" + chars + "' src='images/apple/apple_" + hexLower + ".png'></td>"
+                        + "<td><img class='imgb' alt='" + chars + "' src='images/android/android_" + hexLower + ".png'></td>"
+                        + "<td>" + GenerateEmoji.getName(chars, false) + "</td>"
+                        + "<td>"
+                        );
+                firstText = true;
+            } else {
+                if (firstText) {
+                    out.println("<div>");
+                    firstText = false;
+                } else {
+                    out.println("<div style='margin-top:6pt'>");
+                }
+                out.println(line + "</div>");
+            }
+        }
+        GenerateEmoji.writeFooter(out);
+        out.close();
+        System.out.println("DONE");
+    }
+}
