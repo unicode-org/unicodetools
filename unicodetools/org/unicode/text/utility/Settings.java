912aeef12db6aab79dc1a9940994504c0c99782d
Mark Davis <mark@macchiato.com>
2015-05-28 10:55:56 +0000

    fix problem where the number of scripts passed a threshold, causing an overlap bug
    git-svn-id: https://unicode.org/repos/unicodetools/trunk@846 13e8329f-0b23-4da4-9fe8-d0f6fe080806

--- unicodetools/org/unicode/text/utility/Settings.java
+++ unicodetools/org/unicode/text/utility/Settings.java
@@ -20,7 +20,6 @@ public class Settings {
     public static final String IDN_DIR = DATA_DIR + "IDN/";
     public static final String DICT_DIR = DATA_DIR + "dict/";
     
-    public static final String CHARTS_GEN_DIR = DATA_DIR + "charts/";
 
     public static final String GEN_DIR = Utility.fixFileName(CldrUtility.getProperty("GEN_DIR", OTHER_WORKSPACE_DIRECTORY+"Generated")) + "/";
     public static final String BIN_DIR = GEN_DIR + "BIN/";
@@ -29,6 +28,8 @@ public class Settings {
     public static final String UNICODETOOLS_DIRECTORY = SVN_WORKSPACE_DIRECTORY + "unicodetools/";
     public static final String UNICODE_DRAFT_DIRECTORY = SVN_WORKSPACE_DIRECTORY + "unicode-draft/";
     public static final String UNICODE_DRAFT_PUBLIC = SVN_WORKSPACE_DIRECTORY + "unicode-draft/Public/";
+
+    public static final String CHARTS_GEN_DIR = UNICODE_DRAFT_DIRECTORY + "charts/";
 
     public static final String SRC_DIR = Utility.fixFileName("org/unicode/text") + "/";
     public static final String SRC_UCA_DIR = SRC_DIR + "UCA/";
