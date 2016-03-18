5c278e7994f31cbcfcc3ce7cab6627fe60e6edcb
Mark Davis <mark@macchiato.com>
2016-03-18 17:17:57 +0000

    git-svn-id: https://unicode.org/repos/unicodetools/trunk@1005 13e8329f-0b23-4da4-9fe8-d0f6fe080806

--- UnicodeJsps/src/org/unicode/jsp/SequenceData.java
+++ UnicodeJsps/src/org/unicode/jsp/SequenceData.java
@@ -2,10 +2,13 @@ package org.unicode.jsp;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import org.unicode.cldr.draft.FileUtilities;
 
-import com.google.common.base.Splitter;
+
+
+// import com.google.common.base.Splitter;
 import com.ibm.icu.dev.util.UnicodeMap;
 import com.ibm.icu.impl.Utility;
 import com.ibm.icu.lang.CharSequences;
@@ -16,8 +19,10 @@ public class SequenceData {
     static final UnicodeSet MARK = new UnicodeSet("[:M:]").freeze();
     static final UnicodeSet MODIFIER_BASE = new UnicodeSet("[:emoji_modifier_base:]").freeze();
 
-    static final Splitter SEMI = Splitter.onPattern("[;#]").trimResults();
-    static final Splitter COMMA = Splitter.on(",").trimResults();
+//    static final Splitter SEMI = Splitter.onPattern("[;#]").trimResults();
+//    static final Splitter COMMA = Splitter.on(",").trimResults();
+    static final Pattern SEMI = Pattern.compile("\\s*[;#]\\s*");
+    static final Pattern COMMA = Pattern.compile("\\s*,\\s");
 
     static final UnicodeSet EMOJI_MODIFIER_SEQUENCES = new UnicodeSet();
     static final UnicodeSet EMOJI_ZWJ_SEQUENCES = new UnicodeSet();
@@ -34,7 +39,7 @@ public class SequenceData {
                 if (line.startsWith("#") || line.isEmpty()) {
                     continue;
                 }
-                List<String> list = SEMI.splitToList(line);
+                List<String> list = Arrays.asList(SEMI.split(line));
                 String source = Utility.fromHex(list.get(0), 4, " ");
                 int first = source.codePointAt(0);
                 if (zwj) {
@@ -63,7 +68,7 @@ public class SequenceData {
         }
         for (String line : FileUtilities.in(SequenceData.class, "StandardizedVariants.txt")) {
             if (line.startsWith("#") || line.isEmpty()) continue;
-            List<String> list = SEMI.splitToList(line);
+            List<String> list = Arrays.asList(SEMI.split(line));
             String source = Utility.fromHex(list.get(0), 4, " ");
             int first = source.codePointAt(0);
             int second = source.codePointAt(Character.charCount(first));
