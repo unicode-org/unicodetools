9839930fb56fff33296b7c073af1d20547919956
Mark Davis <mark@macchiato.com>
2016-06-03 10:20:28 +0000

    cleanup GAZ file
    git-svn-id: https://unicode.org/repos/unicodetools/trunk@1063 13e8329f-0b23-4da4-9fe8-d0f6fe080806

--- unicodetools/org/unicode/tools/GlueAfterZwj.java
+++ unicodetools/org/unicode/tools/GlueAfterZwj.java
@@ -10,11 +10,23 @@ import org.unicode.tools.emoji.EmojiData;
 import com.ibm.icu.text.UnicodeSet;
 
 class GlueAfterZwj {
-    static final UnicodeSet GLUE_AFTER_ZWJ = new UnicodeSet(); 
+    static final UnicodeSet GLUE_AFTER_ZWJ = new UnicodeSet();
+    static final String HEADER;
+    
     static {
+        StringBuilder header = new StringBuilder();
+        boolean inHeader = true;
         for (String line : FileUtilities.in(Settings.DATA_DIR + "cldr/","GlueAfterZwj.txt")) {
             // U+02704  ; Glue_After_Zwj #  ✄   WHITE SCISSORS
-            if (line.startsWith("#") || line.isEmpty()) continue;
+            if (line.startsWith("#") || line.isEmpty()) {
+                if (inHeader) {
+                    header.append(line).append("\n");
+                    if (line.startsWith("# DATA")) {
+                        inHeader = false;
+                    }
+                }
+                continue;
+            }
             List<String> coreList = EmojiData.hashOnly.splitToList(line);
             List<String> list = EmojiData.semi.splitToList(coreList.get(0));
             final String f0 = list.get(0);
@@ -29,5 +41,6 @@ class GlueAfterZwj {
             GLUE_AFTER_ZWJ.add(codePoint,codePointEnd);
         }
         GLUE_AFTER_ZWJ.freeze();
+        HEADER = header.toString();
     }
 }
