81013f14238242f733f9a729d8b88ea5576ffce9
Mark Davis <mark@macchiato.com>
2016-06-13 10:16:39 +0000

    change code to only write file if different (other than date, etc.)
    git-svn-id: https://unicode.org/repos/unicodetools/trunk@1078 13e8329f-0b23-4da4-9fe8-d0f6fe080806

---
+++ unicodetools/org/unicode/tools/emoji/TempPrintWriter.java
@@ -0,0 +1,58 @@
+package org.unicode.tools.emoji;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.util.Random;
+
+import org.unicode.cldr.draft.FileUtilities;
+import org.unicode.text.utility.Utility;
+
+import com.ibm.icu.util.ICUUncheckedIOException;
+
+public class TempPrintWriter extends Writer {
+    final PrintWriter tempPrintWriter;
+    final String tempName;
+    final String filename;
+
+    public TempPrintWriter(String dir, String filename) {
+        this(new File(dir, filename));
+    }
+
+    public TempPrintWriter(File file) {
+        final String parentFile = file.getParent();
+        this.filename = file.toString();
+        Random rand = new Random();
+        try {
+            File tempFile;
+            do {
+                tempFile = new File(parentFile, rand.nextInt() + "-" + file.getName());
+            } while (tempFile.exists());
+            tempName = tempFile.toString();
+            tempPrintWriter = FileUtilities.openUTF8Writer(parentFile, tempFile.getName());
+        } catch (IOException e) {
+            throw new ICUUncheckedIOException(e);
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        tempPrintWriter.close();
+        Utility.replaceDifferentOrDelete(filename, tempName, false);
+    }
+
+    @Override
+    public void write(char[] cbuf, int off, int len) throws IOException {
+        tempPrintWriter.write(cbuf, off, len);
+    }
+
+    @Override
+    public void flush() throws IOException {
+        tempPrintWriter.flush();
+    }
+
+    public void println(String line) throws IOException {
+        tempPrintWriter.println(line);
+    }
+}
