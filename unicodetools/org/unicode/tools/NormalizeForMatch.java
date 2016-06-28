38d0df5f4d4831f61bc585553ccc906a1a5ba429
Mark Davis <mark@macchiato.com>
2016-06-28 13:09:31 +0000

    add proposed decompositions.
    git-svn-id: https://unicode.org/repos/unicodetools/trunk@1093 13e8329f-0b23-4da4-9fe8-d0f6fe080806

--- unicodetools/org/unicode/tools/NormalizeForMatch.java
+++ unicodetools/org/unicode/tools/NormalizeForMatch.java
@@ -12,6 +12,7 @@ import org.unicode.props.UcdPropertyValues.Age_Values;
 import org.unicode.props.UcdPropertyValues.General_Category_Values;
 import org.unicode.text.UCD.DerivedProperty;
 import org.unicode.text.utility.Utility;
+import org.unicode.tools.NormalizeForMatch.SpecialReason;
 
 import com.google.common.base.Splitter;
 import com.ibm.icu.dev.util.UnicodeMap;
@@ -19,28 +20,33 @@ import com.ibm.icu.text.UnicodeSet;
 import com.ibm.icu.util.ICUUncheckedIOException;
 
 public class NormalizeForMatch {
+    public enum SpecialReason {digraph, final_form, missing_NFKC, fixed_NFKC, radical}
+
     public static final Splitter SEMI = Splitter.on(";").trimResults();
     public static final Splitter HASH = Splitter.on("#").trimResults();
 
     private final UnicodeMap<String> sourceToTarget;
-    private final UnicodeMap<String> sourceToReason;
+    private final UnicodeMap<SpecialReason> sourceToReason;
 
     public UnicodeMap<String> getSourceToTarget() {
         return sourceToTarget;
     }
 
-    public UnicodeMap<String> getSourceToReason() {
+    public UnicodeMap<SpecialReason> getSourceToReason() {
         return sourceToReason;
     }
 
-    public NormalizeForMatch(UnicodeMap<String> sourceToTarget2, UnicodeMap<String> sourceToReason2) {
+    public NormalizeForMatch(UnicodeMap<String> sourceToTarget2, UnicodeMap<SpecialReason> sourceToReason2) {
         sourceToTarget = sourceToTarget2.freeze();
         sourceToReason = sourceToReason2.freeze();
     }
 
+    static String TEST = Utility.fromHex("1F19B");
+
     public static NormalizeForMatch load(String file) {
         UnicodeMap<String> sourceToTarget = new UnicodeMap<>();
-        UnicodeMap<String> sourceToReason = new UnicodeMap<>();
+        UnicodeMap<SpecialReason> sourceToReason = new UnicodeMap<>();
+        NormalizeForMatch.SpecialReason overrideReason = null;
         try (BufferedReader in = FileUtilities.openFile(NormalizeForMatch.class, file)) {
             while (true) {
                 String line = in.readLine();
@@ -51,10 +57,19 @@ public class NormalizeForMatch {
                 if (clean.isEmpty()) {
                     continue;
                 }
+                if (clean.startsWith("@")) {
+                    overrideReason = SpecialReason.valueOf(line.split("=")[1]);
+                    continue;
+                }
+
                 List<String> parts = SEMI.splitToList(clean);
                 String source = Utility.fromHex(parts.get(0));
+                if (source.equals(TEST)) {
+                    boolean debug = true;
+                }
                 String target = Utility.fromHex(parts.get(1));
-                String reason = parts.get(2);
+                SpecialReason reason =  parts.size() > 2 ? NormalizeForMatch.SpecialReason.valueOf(parts.get(2)) : overrideReason;
+
                 sourceToTarget.put(source, target);
                 sourceToReason.put(source, reason);
             }
@@ -65,7 +80,7 @@ public class NormalizeForMatch {
     }
 
     public static void main(String[] args) {
-        NormalizeForMatch.load("XNFKCCF.txt");
+        NormalizeForMatch curated = NormalizeForMatch.load("XNFKCCF-Curated.txt");
         IndexUnicodeProperties latest = IndexUnicodeProperties.make(Age_Values.V9_0);
         UnicodeSet newCharsOnly = latest.loadEnum(UcdProperty.Age, Age_Values.class).getSet(Age_Values.V9_0);
         UnicodeMap<String> NFKC_Casefold = latest.load(UcdProperty.NFKC_Casefold);
@@ -76,13 +91,19 @@ public class NormalizeForMatch {
                 if (gc.get(s) != gcv) {
                     continue;
                 }
+                if (s.equals(TEST)) {
+                    boolean debug = true;
+                }
+
                 if (first) {
                     System.out.println("\n# GC=" + gcv);
                     first = false;
                 }
-                String nfkccf = NFKC_Casefold.get(s);            
+                String target = curated.getSourceToTarget().get(s);
+                //String target = NFKC_Casefold.get(s);            
                 System.out.println(Utility.hex(s) + "; " + latest.getName(s, " + ")
-                        + (nfkccf == null ? "" : "; " + Utility.hex(nfkccf, " ") + "; " + latest.getName(nfkccf, " + ")));
+                        + (target == null ? "" : "; " + Utility.hex(target, " ")
+                                + "; " + latest.getName(target, " + ")));
             }
         }
     }
