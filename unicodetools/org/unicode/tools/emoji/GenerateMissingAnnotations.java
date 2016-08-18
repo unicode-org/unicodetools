2d770a94fc9a3b190e09a2ee7e964a5403ee9d8f
Mark Davis <mark@macchiato.com>
2016-08-18 08:45:02 +0000

    small tweaks for charts
    git-svn-id: https://unicode.org/repos/unicodetools/trunk@1135 13e8329f-0b23-4da4-9fe8-d0f6fe080806

--- unicodetools/org/unicode/tools/emoji/GenerateMissingAnnotations.java
+++ unicodetools/org/unicode/tools/emoji/GenerateMissingAnnotations.java
@@ -2,8 +2,10 @@ package org.unicode.tools.emoji;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.EnumSet;
+import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Locale;
 import java.util.Map;
@@ -15,6 +17,7 @@ import java.util.TreeSet;
 import org.unicode.cldr.draft.FileUtilities;
 import org.unicode.cldr.util.Annotations;
 import org.unicode.cldr.util.CLDRConfig;
+import org.unicode.cldr.util.Counter;
 import org.unicode.cldr.util.Level;
 import org.unicode.cldr.util.Organization;
 import org.unicode.text.utility.Settings;
@@ -22,11 +25,107 @@ import org.unicode.text.utility.Utility;
 import org.unicode.tools.emoji.Emoji.Source;
 import org.unicode.tools.emoji.EmojiAnnotations.Status;
 
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
 import com.ibm.icu.dev.util.CollectionUtilities;
+import com.ibm.icu.impl.Row.R2;
 import com.ibm.icu.text.UnicodeSet;
 
 public class GenerateMissingAnnotations {
     public static void main(String[] args) throws IOException {
+        countWords();
+        //generateMissing();
+    }
+
+    private static void countWords() {
+        Multimap<String,String> orderWordToCps = HashMultimap.create();
+        Counter<String> orderCounter = new Counter<>();
+
+        for (Entry<String, Set<String>> entry : EmojiOrder.STD_ORDER.orderingToCharacters.keyValuesSet()) {
+            final String orderWords = entry.getKey();
+            final Set<String> cps = entry.getValue();
+            System.out.println(orderWords + "\t" + cps);
+            for (String orderWord : orderWords.split("[-\\s]+")) {
+                for (String cp : cps) {
+                    if (EmojiData.MODIFIERS.containsSome(cp)) {
+                        continue;
+                    }
+                    if (!cp.equals(EmojiData.EMOJI_DATA.addEmojiVariants(cp))) {
+                        continue; // defective
+                    }
+                    orderCounter.add(orderWord, 1);
+                    orderWordToCps.put(orderWord, cp);
+                }
+            }
+        }
+
+        Counter<String> keywordCounter = new Counter<>();
+        Multimap<String,String> keywordToCps = HashMultimap.create();
+        EmojiAnnotations em = new EmojiAnnotations("en", EmojiOrder.STD_ORDER.codepointCompare);
+        for (String cp: em.getStatusKeys()) {
+            final Status status = em.getStatus(cp);
+            if (status == Status.missing || status == Status.constructed) {
+                continue;
+            }
+            if (EmojiData.MODIFIERS.containsSome(cp)) {
+                continue;
+            }
+            for (String keyword : em.getKeys(cp)) {
+                keywordCounter.add(keyword, 1);
+                keywordToCps.put(keyword, cp);
+            }
+        }
+        Set<String> sortedKeyCps = new TreeSet<>(EmojiOrder.STD_ORDER.codepointCompare);
+        Set<String> sortedOrderCps = new TreeSet<>(EmojiOrder.STD_ORDER.codepointCompare);
+        Set<String> same = new TreeSet<>(EmojiOrder.STD_ORDER.codepointCompare);
+        System.out.println("\nKeywords\n");
+        for (R2<Long, String> s : keywordCounter.getEntrySetSortedByCount(false, null)) {
+            final Long count = s.get0();
+            if (count < 2) continue;
+            final String keyword = s.get1();
+            if (keyword.equals("flag")) {
+                int debug = 0;
+            }
+            final Collection<String> keywordCps = keywordToCps.get(keyword);
+            sortedKeyCps.clear();
+            sortedKeyCps.addAll(keywordCps);
+            System.out.print(count + "\t" + keyword + "\t" + sortedKeyCps);
+            Collection<String> orderCps = orderWordToCps.get(keyword);
+            if (orderCps != null) {
+                sortedOrderCps.clear();
+                sortedOrderCps.addAll(orderCps);
+                if (sortedKeyCps.equals(sortedOrderCps)) {
+                    System.out.print("\n\tEQUAL");
+                } else {
+                    same.clear();
+                    same.addAll(sortedKeyCps);
+                    same.retainAll(sortedOrderCps);
+                    sortedKeyCps.removeAll(same);
+                    sortedOrderCps.removeAll(same);
+                    System.out.print("\n\tK-O:\t" + sortedKeyCps.size() + "\t" + sortedKeyCps);
+                    System.out.print("\n\tO-K:\t" + sortedOrderCps.size() + "\t" + sortedOrderCps);
+                }
+            }
+            System.out.println();
+        }
+        for (R2<Long, String> s : keywordCounter.getEntrySetSortedByCount(false, null)) {
+            final Long count = s.get0();
+            if (count < 5) continue;
+            final String keyword = s.get1();
+            if (!orderWordToCps.containsKey(keyword)) {
+                System.out.println("Not in Order:\t" + count + "\t" + keyword);
+            }
+        }
+        for (R2<Long, String> s : orderCounter.getEntrySetSortedByCount(false, null)) {
+            final Long count = s.get0();
+            final String keyword = s.get1();
+            if (!keywordToCps.containsKey(keyword)) {
+                System.out.println("Not in Keywords:\t" + count + "\t" + keyword);
+            }
+        }
+    }
+
+    private static void generateMissing() throws IOException {
         final CLDRConfig config = CLDRConfig.getInstance();
         Set<String> locales = config.getStandardCodes().getLocaleCoverageLocales(Organization.google, EnumSet.of(Level.MODERN));
 
@@ -56,7 +155,7 @@ public class GenerateMissingAnnotations {
                         + "\t" + em.getShortName(s));
             }
         }
-        
+
         Set<String> skipped = new LinkedHashSet<>();
         Map<String, Counts> countMap = new TreeMap<>();
         for (String s : locales) {
@@ -86,7 +185,7 @@ public class GenerateMissingAnnotations {
     static final class Counts {
         int emojiCount;
         int charCount;
-        
+
         void add(String tts, Set<String> keywords) {
             ++emojiCount;
             // rough char count
@@ -101,7 +200,7 @@ public class GenerateMissingAnnotations {
             return emojiCount + "\t" + charCount;
         }
     }
-    
+
     private static String getKey(String s) {
         return "_" + Utility.hex(s,"_").toLowerCase(Locale.ROOT).replace("_fe0f", "");
     }
@@ -140,7 +239,7 @@ public class GenerateMissingAnnotations {
         if (!missing.isEmpty()) {
             if (em2 != null) {
                 out.println(localeStr
-                        + "\tImage\tEnglish Name\tEnglish Keywords\tNative Name\tNative Keywords\tName Name (constructed!)\tNative Keywords (constructed!)\t-");
+                        + "\tImage\tEnglish Name\tEnglish Keywords\tNative Name\tNative Keywords\tName (constructed!)\tNative Keywords (constructed!)\t-");
             }
             for (String s : missing) {
                 out.println(s);
