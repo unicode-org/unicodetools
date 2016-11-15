818a7c7e5fd0157b3aadee831a3d9ea1e0977d58
Mark Davis <mark@macchiato.com>
2016-11-15 00:58:33 +0000

    reenable the cvs files
    git-svn-id: https://unicode.org/repos/unicodetools/trunk@1171 13e8329f-0b23-4da4-9fe8-d0f6fe080806

--- unicodetools/org/unicode/tools/emoji/GenerateEmojiKeyboard.java
+++ unicodetools/org/unicode/tools/emoji/GenerateEmojiKeyboard.java
@@ -20,7 +20,7 @@ public class GenerateEmojiKeyboard {
 
     public static void main(String[] args) throws Exception {
         GenerateEmojiKeyboard.showLines(EmojiOrder.STD_ORDER, Target.propFile, Emoji.DATA_DIR);
-        // GenerateEmojiKeyboard.showLines(EmojiOrder.STD_ORDER, Target.csv, Emoji.TR51_INTERNAL_DIR + "keyboard");
+        GenerateEmojiKeyboard.showLines(EmojiOrder.STD_ORDER, Target.csv, Emoji.TR51_INTERNAL_DIR + "keyboard");
         //        boolean foo2 = EmojiData.EMOJI_DATA.getChars().contains(EmojiData.SAMPLE_WITHOUT_TRAILING_EVS);
         //        Set<String> foo = EmojiOrder.sort(EmojiOrder.STD_ORDER.codepointCompare, 
         //                EmojiData.EMOJI_DATA.getChars());
