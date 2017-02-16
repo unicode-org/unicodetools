8f2641b5e2f78017c621e2d7d17af161e4d6b0c4
Mark Davis <mark@macchiato.com>
2017-02-16 08:49:10 +0000

    update after Markus's latest
    git-svn-id: https://unicode.org/repos/unicodetools/trunk@1208 13e8329f-0b23-4da4-9fe8-d0f6fe080806

--- unicodetools/org/unicode/props/VersionToAge.java
+++ unicodetools/org/unicode/props/VersionToAge.java
@@ -9,56 +9,57 @@ import org.unicode.props.UcdPropertyValues.Age_Values;
 import com.google.common.collect.ImmutableMap;
 
 public class VersionToAge {
-    private static Map<Age_Values, Long> versionToDate 
-    = ImmutableMap.<Age_Values, Long>builder()
-    .put(Age_Values.V9_0, getDate(2016, 6))
-    .put(Age_Values.V8_0, getDate(2015, 6))
-    .put(Age_Values.V7_0, getDate(2014, 6))
-    .put(Age_Values.V6_3, getDate(2013, 9))
-    .put(Age_Values.V6_2, getDate(2012, 9))
-    .put(Age_Values.V6_1, getDate(2012, 1))
-    .put(Age_Values.V6_0, getDate(2010, 10))
-    .put(Age_Values.V5_2, getDate(2009, 10))
-    .put(Age_Values.V5_1, getDate(2008, 4))
-    .put(Age_Values.V5_0, getDate(2006, 7))
-    .put(Age_Values.V4_1, getDate(2005, 3))
-    .put(Age_Values.V4_0, getDate(2003, 4))
-    .put(Age_Values.V3_2, getDate(2002, 3))
-    .put(Age_Values.V3_1, getDate(2001, 8))
-    .put(Age_Values.V3_0, getDate(2000, 8))
-    .put(Age_Values.V2_1, getDate(1999, 4))
-    .put(Age_Values.V2_0, getDate(1996, 7))
-    .put(Age_Values.V1_1, getDate(1995, 7))
-    .put(Age_Values.Unassigned, Long.MAX_VALUE)
-    .build();
-    public static Long getDate(int year, int month) {
-        return new Date(year-1900, month-1, 1).getTime();
-    }
-    public static Date getDate(Age_Values version) {
-        return new Date(versionToDate.get(version));
-    }
-    public static int getYear(Age_Values versionInfo) {
-        return getDate(versionInfo).getYear()+1900;
-    }
-    public static Age_Values getAge(long requestedDate) {
-        //System.out.println("req-date=" + new Date(requestedDate));
-        Age_Values best = Age_Values.V1_1;
-        // Find the latest version that is at or before requestedDate
-        for (Entry<Age_Values, Long> entry : versionToDate.entrySet()) {
-            //System.out.println("entry-date=" + new Date(entry.getValue()));
-            if (entry.getValue() <= requestedDate
-                    && entry.getKey().compareTo(best) > 0) {
-                best = entry.getKey();
-            }
-        }
-        return best;
-    }
-    public static void main(String[] args) {
-        Age_Values age = getAge(new Date(2015-1900,11,31,23,59,59).getTime());
-        System.out.println(age);
-        for (Age_Values x : Age_Values.values()) {
-            System.out.println(x + "\t" + getYear(x));
-        }
-    }
+	private static Map<Age_Values, Long> versionToDate 
+	= ImmutableMap.<Age_Values, Long>builder()
+	.put(Age_Values.V10_0, getDate(2017, 6))
+	.put(Age_Values.V9_0, getDate(2016, 6))
+	.put(Age_Values.V8_0, getDate(2015, 6))
+	.put(Age_Values.V7_0, getDate(2014, 6))
+	.put(Age_Values.V6_3, getDate(2013, 9))
+	.put(Age_Values.V6_2, getDate(2012, 9))
+	.put(Age_Values.V6_1, getDate(2012, 1))
+	.put(Age_Values.V6_0, getDate(2010, 10))
+	.put(Age_Values.V5_2, getDate(2009, 10))
+	.put(Age_Values.V5_1, getDate(2008, 4))
+	.put(Age_Values.V5_0, getDate(2006, 7))
+	.put(Age_Values.V4_1, getDate(2005, 3))
+	.put(Age_Values.V4_0, getDate(2003, 4))
+	.put(Age_Values.V3_2, getDate(2002, 3))
+	.put(Age_Values.V3_1, getDate(2001, 8))
+	.put(Age_Values.V3_0, getDate(2000, 8))
+	.put(Age_Values.V2_1, getDate(1999, 4))
+	.put(Age_Values.V2_0, getDate(1996, 7))
+	.put(Age_Values.V1_1, getDate(1995, 7))
+	.put(Age_Values.Unassigned, Long.MAX_VALUE)
+	.build();
+	public static Long getDate(int year, int month) {
+		return new Date(year-1900, month-1, 1).getTime();
+	}
+	public static Date getDate(Age_Values version) {
+		return new Date(versionToDate.get(version));
+	}
+	public static int getYear(Age_Values versionInfo) {
+		return getDate(versionInfo).getYear()+1900;
+	}
+	public static Age_Values getAge(long requestedDate) {
+		//System.out.println("req-date=" + new Date(requestedDate));
+		Age_Values best = Age_Values.V1_1;
+		// Find the latest version that is at or before requestedDate
+		for (Entry<Age_Values, Long> entry : versionToDate.entrySet()) {
+			//System.out.println("entry-date=" + new Date(entry.getValue()));
+			if (entry.getValue() <= requestedDate
+					&& entry.getKey().compareTo(best) > 0) {
+				best = entry.getKey();
+			}
+		}
+		return best;
+	}
+	public static void main(String[] args) {
+		Age_Values age = getAge(new Date(2015-1900,11,31,23,59,59).getTime());
+		System.out.println(age);
+		for (Age_Values x : Age_Values.values()) {
+			System.out.println(x + "\t" + getYear(x));
+		}
+	}
 }
 
