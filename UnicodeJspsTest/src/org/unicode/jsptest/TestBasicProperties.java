830c594322dce9baedf064142927eb1297a6b822
Mark Davis <mark@macchiato.com>
2017-04-17 17:20:05 +0000

    update for properties
    git-svn-id: https://unicode.org/repos/unicodetools/trunk@1269 13e8329f-0b23-4da4-9fe8-d0f6fe080806

--- UnicodeJspsTest/src/org/unicode/jsptest/TestBasicProperties.java
+++ UnicodeJspsTest/src/org/unicode/jsptest/TestBasicProperties.java
@@ -1,9 +1,13 @@
 package org.unicode.jsptest;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Locale;
 import java.util.Set;
 import java.util.TreeSet;
 
+import org.unicode.jsp.CachedProps;
 import org.unicode.jsp.PropertyMetadata;
 import org.unicode.jsp.UnicodeProperty;
 import org.unicode.jsp.XPropertyFactory;
@@ -11,6 +15,7 @@ import org.unicode.jsp.XPropertyFactory;
 import com.ibm.icu.impl.Row.R4;
 import com.ibm.icu.text.Collator;
 import com.ibm.icu.util.ULocale;
+import com.ibm.icu.util.VersionInfo;
 
 public class TestBasicProperties extends TestFmwk2 {
 	public static void main(String[] args) {
@@ -35,9 +40,20 @@ public class TestBasicProperties extends TestFmwk2 {
 		for (String name : factory.getAvailableNames()) {
 			UnicodeProperty prop = factory.getProperty(name);
     		String value = prop.getValue(sample.codePointAt(0));
-			System.out.println(name + "\tvalue('a') = " + value);
+			logln(name + "\tvalue('a') = " + value);
 			assertTrue("has PropertyMetadata: " + name, withPropData.contains(name.toUpperCase(Locale.ENGLISH)));
 		}
 	}
 	
+	public void TestPropertyMetadata() {
+        Set<String> hasMetadata = new TreeSet<String>();
+        for (R4<String, String, String, String> propData : PropertyMetadata.CategoryDatatypeSourceProperty) {
+            String propName = propData.get3();
+            hasMetadata.add(propName);
+        }
+        CachedProps cp = CachedProps.getInstance(VersionInfo.getInstance(10));
+        Set<String> propsMissingMetadata = new LinkedHashSet<String>(cp.getPropertyNames());
+        propsMissingMetadata.removeAll(hasMetadata);
+        assertEquals("PropertyMetadata", Collections.EMPTY_SET, propsMissingMetadata);
+	}
 }
