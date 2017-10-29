b9e7acc4876c7faa00abb284ed16ce31a5509140
Mark Davis <mark@macchiato.com>
2017-10-29 06:01:55 +0000

    git-svn-id: https://unicode.org/repos/unicodetools/trunk@1346 13e8329f-0b23-4da4-9fe8-d0f6fe080806

---
+++ unicodetools/org/unicode/propstest/CheckFaces.java
@@ -0,0 +1,39 @@
+package org.unicode.propstest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.unicode.props.IndexUnicodeProperties;
+import org.unicode.props.UcdProperty;
+import org.unicode.text.utility.Utility;
+
+import com.ibm.icu.dev.util.UnicodeMap;
+import com.ibm.icu.text.UnicodeSet;
+
+public class CheckFaces {
+    public static void main(String[] args) {
+        IndexUnicodeProperties iup = IndexUnicodeProperties.make();
+        UnicodeMap<String> names = iup.load(UcdProperty.Name);
+        Map<String, UnicodeSet> nameToUset = new HashMap<>();
+
+        UnicodeSet faces = new UnicodeSet();
+        for (String name : names.values()) {
+            UnicodeSet uset = names.getSet(name);
+            nameToUset.put(name, uset.freeze());
+            if (name.contains("FACE")) {
+                faces.addAll(uset);
+            }
+        }
+        for (String s : faces) {
+            String name = names.get(s);
+            String noface = name.replace("FACE", "").trim().replace("  ", " ");
+            UnicodeSet others = nameToUset.get(noface);
+            if (others != null) {
+                System.out.println("U+" + Utility.hex(s) 
+                        + "\t" + name 
+                        + "\t" + "U+" + Utility.hex(others.iterator().next()) 
+                        + "\t" + noface);
+            }
+        }
+    }
+}
