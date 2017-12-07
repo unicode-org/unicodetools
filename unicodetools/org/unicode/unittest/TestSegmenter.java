49a77b113cbb0109c789294a6b7f1668dab8ea88
Mark Davis <mark@macchiato.com>
2017-12-07 08:27:27 +0000

    git-svn-id: https://unicode.org/repos/unicodetools/trunk@1370 13e8329f-0b23-4da4-9fe8-d0f6fe080806

--- unicodetools/org/unicode/unittest/TestSegmenter.java
+++ unicodetools/org/unicode/unittest/TestSegmenter.java
@@ -1,5 +1,6 @@
 package org.unicode.unittest;
 
+import org.unicode.cldr.draft.FileUtilities;
 import org.unicode.props.IndexUnicodeProperties;
 import org.unicode.props.UcdProperty;
 import org.unicode.props.UcdPropertyValues.Binary;
@@ -9,20 +10,21 @@ import org.unicode.tools.Segmenter;
 import org.unicode.tools.Segmenter.Builder;
 
 import com.ibm.icu.dev.test.TestFmwk;
+import com.ibm.icu.impl.locale.XCldrStub.Splitter;
 import com.ibm.icu.text.UnicodeSet;
 
 public class TestSegmenter extends TestFmwk{
     public static void main(String[] args) {
         IndexUnicodeProperties iup = IndexUnicodeProperties.make();
-        UnicodeSet emoji = iup
-                .loadEnum(UcdProperty.Emoji, Binary.class)
-                .getSet(Binary.Yes);
-        System.out.println(emoji.size() + "\t" + emoji.toPattern(false));
-
-        UnicodeSet extended_pictographic = iup
-                .loadEnum(UcdProperty.Extended_Pictographic, Binary.class)
-                .getSet(Binary.Yes);
-        System.out.println(extended_pictographic.size() + "\t" + extended_pictographic.toPattern(false));
+//        UnicodeSet emoji = iup
+//                .loadEnum(UcdProperty.Emoji, Binary.class)
+//                .getSet(Binary.Yes);
+//        System.out.println(emoji.size() + "\t" + emoji.toPattern(false));
+//
+//        UnicodeSet extended_pictographic = iup
+//                .loadEnum(UcdProperty.Extended_Pictographic, Binary.class)
+//                .getSet(Binary.Yes);
+//        System.out.println(extended_pictographic.size() + "\t" + extended_pictographic.toPattern(false));
 
         new TestSegmenter().run(args);
     }
@@ -46,5 +48,29 @@ public class TestSegmenter extends TestFmwk{
                 }
             }
         }
+    }
+    
+    public void TestIndic() {
+        System.out.println();
+        int lineCount = 0;
+        Splitter spaceSplitter = Splitter.on(' ').trimResults();
+        for (String line : FileUtilities.in(TestSegmenter.class, "TestSegmenter.txt")) {
+            for (String part : spaceSplitter.split(line)) {
+                lineCount = showBreakLines(lineCount, part);
+            }
+        }
+    }
+
+    private int showBreakLines(int lineCount, String line) {
+        StringBuilder buffer = new StringBuilder();
+        buffer.append(line.charAt(0));
+        for (int i = 1; i < line.length(); ++i) {
+            if (gcb.breaksAt(line, i)) {
+                buffer.append('➗');
+            }
+            buffer.append(line.charAt(i));
+        }
+        System.out.println(++lineCount + " ; \t" + line + " ; \t" + buffer);
+        return lineCount;
     }
 }
