9e5cc698d723dca7cf05dd5ef0a5b599e994c625
Mark Davis <mark@macchiato.com>
2018-09-09 08:13:42 +0000

    git-svn-id: https://unicode.org/repos/unicodetools/trunk@1550 13e8329f-0b23-4da4-9fe8-d0f6fe080806

--- unicodetools/org/unicode/tools/emoji/GenerateEmojiFrequency.java
+++ unicodetools/org/unicode/tools/emoji/GenerateEmojiFrequency.java
@@ -6,23 +6,24 @@ import com.ibm.icu.impl.Row.R2;
 
 public class GenerateEmojiFrequency {
     public static void main(String[] args) {
-        EmojiOrder order = EmojiOrder.of(Emoji.VERSION_LAST_RELEASED);
-
-        for (Entry<String, Long> entry : ListEmojiGroups.EmojiTracker.countInfo.keyToCount.entrySet()) {
-            long count = entry.getValue();
-            String str = entry.getKey();
-
-            String category = order.getCategory(str);
-
-            System.out.println(str
-                    //+ "\tU+" + m.group(1)
-                    + "\t" + EmojiData.EMOJI_DATA.getName(str)
-                    + "\t" + count
-                    + "\t" + order.getMajorGroupFromCategory(category).toPlainString()
-                    + "\t" + category
-                    + "\t" + order.getGroupOrder(category)
-                    );
-
-        }
+        ListEmojiGroups.main(args);
+//        EmojiOrder order = EmojiOrder.of(Emoji.VERSION_LAST_RELEASED);
+//
+//        for (Entry<String, Long> entry : ListEmojiGroups.EmojiTracker.countInfo.keyToCount.entrySet()) {
+//            long count = entry.getValue();
+//            String str = entry.getKey();
+//
+//            String category = order.getCategory(str);
+//
+//            System.out.println(str
+//                    //+ "\tU+" + m.group(1)
+//                    + "\t" + EmojiData.EMOJI_DATA.getName(str)
+//                    + "\t" + count
+//                    + "\t" + order.getMajorGroupFromCategory(category).toPlainString()
+//                    + "\t" + category
+//                    + "\t" + order.getGroupOrder(category)
+//                    );
+//
+//        }
     }
 }
