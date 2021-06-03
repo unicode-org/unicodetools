163def1d820fae750b11a7f33cf01dd1959f6b35
Mark Davis <mark@macchiato.com>
2021-06-03 16:08:30 -0700

    CLDR 14.0 keyword fixes (#65)
    * CLDR 14.0 keyword fixes
    
    * Add images for the new emoji to CLDR.
    
    * Update candidateData.txt
    
    fix problem introduced by bad merge
    
    * Fix typo (captivate becomes captivated)

---
+++ unicodetools/org/unicode/propstest/CheckEmojiProps.java
@@ -0,0 +1,27 @@
+package org.unicode.propstest;
+
+import java.util.Objects;
+
+import org.unicode.props.IndexUnicodeProperties;
+import org.unicode.props.UcdProperty;
+import org.unicode.text.utility.Utility;
+
+public class CheckEmojiProps {
+    public static void main(String[] args) {
+	IndexUnicodeProperties latest = IndexUnicodeProperties.make();
+	final String unassigned = new StringBuilder().appendCodePoint(0x1FC00).toString();
+	final String sampleEmoji = "🧔";
+
+	System.out.println("Comparing samples: " + sampleEmoji + " to U+" + Utility.hex(unassigned));
+	for (UcdProperty item : UcdProperty.values()) {
+	    try {
+		String cn = latest.getResolvedValue(item, unassigned);
+		String emoji = latest.getResolvedValue(item, sampleEmoji);
+		if (!Objects.equals(emoji, cn)) {
+		    System.out.println(item + "\t" + emoji + "≠" + cn);
+		}
+	    } catch (Exception e) {
+	    }
+	}
+    }
+}
