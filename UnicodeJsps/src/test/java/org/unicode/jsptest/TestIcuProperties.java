343e27eca5002ad73998422652c130c13c231ff0
Steven R. Loomis <srl295@gmail.com>
2022-03-10 17:57:43 -0600

    chore: remove tabs
    For: #146

--- UnicodeJsps/src/test/java/org/unicode/jsptest/TestIcuProperties.java
+++ UnicodeJsps/src/test/java/org/unicode/jsptest/TestIcuProperties.java
@@ -1,4 +1,5 @@
 package org.unicode.jsptest;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,16 +10,15 @@ import com.ibm.icu.lang.UCharacter;
 import com.ibm.icu.lang.UProperty;
 import com.ibm.icu.lang.UProperty.NameChoice;
 
-
 /**
  * "Not really a test, move"
  */
 public class TestIcuProperties extends TestFmwk2 {
     public void testProps() {
-    	ICUPropertyFactory factory = ICUPropertyFactory.make();
-    	String sample = "🤩";
+        ICUPropertyFactory factory = ICUPropertyFactory.make();
+        String sample = "🤩";
 
-    	int nameChoice = NameChoice.LONG;
+        int nameChoice = NameChoice.LONG;
         List<Integer> propRanges = Arrays.asList(
                 UProperty.BINARY_START, UProperty.BINARY_LIMIT,
                 UProperty.INT_START, UProperty.INT_LIMIT,
@@ -26,17 +26,18 @@ public class TestIcuProperties extends TestFmwk2 {
                 UProperty.STRING_START, UProperty.STRING_LIMIT);
         for (int range = 0; range < propRanges.size(); range += 2) {
             final int rangeStart = propRanges.get(range);
-            final int rangeLimit = propRanges.get(range+1);
+            final int rangeLimit = propRanges.get(range + 1);
             for (int property = rangeStart; property < rangeLimit; ++property) {
                 String name = UCharacter.getPropertyName(property, nameChoice);
-        		UnicodeProperty prop = factory.getInternalProperty(name);
-        		String value = prop.getValue(sample.codePointAt(0));
-        		assertTrue("property exists", prop != null);
+                UnicodeProperty prop = factory.getInternalProperty(name);
+                String value = prop.getValue(sample.codePointAt(0));
+                assertTrue("property exists", prop != null);
 
                 System.out.println(property + "\t" + name + "\tvalue('" + sample + "'): " + value);
                 if (rangeStart == UProperty.INT_START) {
                     String gap = "\t ";
-                    for (int i = UCharacter.getIntPropertyMinValue(property); i <= UCharacter.getIntPropertyMaxValue(property); ++i) {
+                    for (int i = UCharacter.getIntPropertyMinValue(property); i <= UCharacter
+                            .getIntPropertyMaxValue(property); ++i) {
                         String propertyValueName = UCharacter.getPropertyValueName(property, i, nameChoice);
                         System.out.print(gap + propertyValueName);
                         gap = ", ";
