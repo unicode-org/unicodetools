f0fb440a81bb949f9ac30ff80f533901e51e3db1
Mark Davis <mark@macchiato.com>
2022-05-06 11:19:21 -0700

    15.0 emoji versions (#232)
    * 15.0 emoji versions
    
    * Update emoji to 15.0
    
    * Add emoji chart destination
    
    * Take second pass to fix E0.0 and generate proper emoji-test.txt (and update the instructions)

--- docs/emoji/index.md
+++ docs/emoji/index.md
@@ -4,12 +4,22 @@
 
 1.  Get the UCD tools updated (see [Building Unicode Tools](../index.md))
 2.  \[If New version\]
-    1.  Set **the version as per instructions in** Emoji.java.
+    1.  Set **the version as per instructions in** the header of Emoji.java.
     2.  Create a new folder **{unicode-draft}**/emoji/**X**.0/
     3.  Copy in the ReadMe.txt from the last version, but add the word "draft"
         in front of "emoji".
+    4. And add the images to the images repository.
+    5. Make sure that docRegistry.txt is up to date.
+      1. Look at the last line in docRegistry.txt, eg L2/20-153 ...
+      2. Go to the https://www.unicode.org/L2/L-curdoc.htm
+      3. Find the year, and copy the first column after L2/20-153 down to the last real line.
+      3. Paste at the end of docRegistry.txt. Make sure there are 4 tab delimited columns.
+      4. Repeat for any later years.
+      5. NOTE: sometimes there are some glytches in the columns. Typically when there are multiple lines in a cell. You'll see that when a line doesn't start with L2/... In that case, fix the lines (typically by joining with previous line)
+    6. *Note that the data files with the new emoji will not be generated until candidateData.txt has Status=Draft Candidate.*
 3.  Run **{unicodetools/org/unicode/text}**/tools/GenerateEmoji.java
     1.  For VM arguments see the section below.
+    2. Sometimes the candidateData.txt file will be malformed, and you'll see some errors. Fix them.
 4.  That will generate updated files in one of two places, according to the
     setting Emoji.IS_BETA.
     1.  **CURRENT VERSION**
@@ -20,8 +30,46 @@
         2.  **{unicode-draft}**/emoji/**charts-NEXT.0**/\*
 5.  Special small versions of the charts are in corresponding directories like:
     1.  **{unicode-draft}**/emoji/🏴charts-11.0/emoji-list.html
-6.  Sanity-check them, diff against old files, and check in.
-    1.  Don't use Eclipse diff for the big chart files: they are too big for it.
+6.  Sanity-check data files, diffing against old files.
+    1. Old data files will be in unicodetools/unicodetools/data/emoji/**CURRENT**.0/
+    2. New data files in **{unicode-draft}**
+    3. If all looks good, copy the new data files into will go into unicodetools/unicodetools/data/emoji/**NEXT**.0/
+7. If you are not doing this the first time:
+    1. Sanity check the new charts (Don't use Eclipse diff for the big chart files: they are too big for it.)
+    2. Copy them into  https://github.com/unicode-org/emoji/tree/main/docs/emoji
+      1. /**NEXT**.0/
+      2. /future/
+
+
+ \[If New version\]
+Some things will not be fixed yet, so you have to take a second pass.
+
+1. Update emojiOrdering.txt
+Diff unicodetools/src/main/resources/org/unicode/tools/emoji/emojiOrdering.txt with the new 
+unicodetools/data/emoji/**NEXT**/internal/emojiOrdering.txt
+Copy in the new characters from internal/emojiOrdering.txt to emoji/emojiOrdering.txt. 
+**Don't** remove the sets of ZWJ sequences like 👱‍♀ 👱‍♀️ 👱‍♂ 👱‍♂️ 👱🏻‍♀ 👱🏻‍♀️ 👱🏻‍♂ 👱🏻‍♂️ 👱🏼‍♀ 👱🏼‍♀️ 👱🏼‍♂ 👱🏼‍♂️ 👱🏽‍♀ 👱🏽‍♀️ ; those are still needed.
+
+2. Run GenerateEmoji.java again.
+If you missed one of the new characters you will probably get an error in building the EmojiOrder. 
+This will update the generated files to have **NEXT** instead of E0.0. Example:
+``
+1F6DC         ; Emoji                # E0.0   [1] (🛜)       wireless
+becomes
+``
+1F6DC         ; Emoji                # E15.0  [1] (🛜)       wireless
+``
+It will also add the new characters to emoji-test.txt (they will be missing in the first pass.)
+
+3. Copy over the data files again
+From: **{unicode-draft}**/Public/emoji/**NEXT.0**/\*
+To: unicodetools/unicodetools/data/emoji/**NEXT**.0/
+
+1. Go to step 7 above to copy the charts.
+2. 
+
+### CLDR
+CLDR uses some files once the correct emoji-test.txt file is built. For details, see https://cldr.unicode.org/development/generate-emoji-paths
 
 ### GenerateEmoji: VM Arguments
 
---
+++ unicodetools/src/test/java/org/unicode/propstest/CheckPropertyStability.java
@@ -0,0 +1,126 @@
+package org.unicode.propstest;
+
+import java.util.Arrays;
+
+import org.unicode.props.IndexUnicodeProperties;
+import org.unicode.props.UcdProperty;
+import org.unicode.props.UcdPropertyValues.Age_Values;
+import org.unicode.props.UcdPropertyValues.General_Category_Values;
+import org.unicode.props.UnicodeProperty;
+import org.unicode.props.VersionToAge;
+import org.unicode.text.utility.Utility;
+
+import com.ibm.icu.dev.util.UnicodeMap;
+import com.ibm.icu.text.UnicodeSet;
+
+/**
+ * Simple tool for checking the stability of a property+value A property value
+ * is stable in a version when the only change is when a new character is
+ * created. In this listing, those are called "new". The unstable changes are
+ * when an existing character changes value: here listed as either "Removed" or
+ * "Changed".
+ * 
+ * <p>The first argument is the property name, and the second is the
+ * property value. Aliases work for both. The default value if not supplied is "Yes".
+ * 
+ * <p>For example, General_Category = Letter_Number (as of V14) was last unstable
+ * in V5.0 2006, with the following: 
+ * <br>REMOVED: 2183 V3_0 ROMAN NUMERAL REVERSED ONE HUNDRED 
+ * <br>CHANGED: 10341 V3_1 GOTHIC LETTER NINETY
+ */
+public class CheckPropertyStability {
+
+    static IndexUnicodeProperties latest = IndexUnicodeProperties.make();
+    static UnicodeMap<Age_Values> birth = latest.loadEnum(UcdProperty.Age, Age_Values.class);
+    static UnicodeSet nonchar = latest.loadBinary(UcdProperty.Noncharacter_Code_Point);
+
+    static UnicodeMap<General_Category_Values> latestGc = latest.loadEnum(UcdProperty.General_Category,
+            General_Category_Values.class);
+    static UnicodeSet surrogate = latestGc.getSet(General_Category_Values.Surrogate);
+    static UnicodeSet privateUse = latestGc.getSet(General_Category_Values.Private_Use);
+    static UnicodeSet control = latestGc.getSet(General_Category_Values.Control);
+    static UnicodeSet unassigned = latestGc.getSet(General_Category_Values.Unassigned);
+
+    public static void main(String[] args) {
+
+        // get the arguments
+
+        UcdProperty prop = args.length < 1 ? UcdProperty.Default_Ignorable_Code_Point : UcdProperty.forString(args[0]);
+        if (prop == null) {
+            System.out.println("Not supported property: " + args[0]);
+            System.out.println("Use one of:\n" + Arrays.asList(UcdProperty.values()));
+            return;
+
+        }
+        UnicodeProperty uprop = latest.getProperty(prop);
+
+        String propValueRaw = args.length < 2 ? latest.getDefaultValue(prop) : args[1];
+
+        // Check that the value occurs.
+
+        UnicodeSet uset = uprop.getSet(propValueRaw, null);
+        if (uset.isEmpty()) {
+            System.out.println("No current code points have the value " + prop + " = " + propValueRaw);
+            System.out.println("Use one of:\n" + uprop.getAvailableValues());
+            return;
+        }
+
+        // get the normalized form of the property
+
+        String propValue = uprop.getValue(uset.charAt(0));
+
+        System.out.println("History of: " + prop + " = " + propValue);
+
+        UnicodeSet lastDi = null;
+        UnicodeSet lastUndefined = null;
+        for (Age_Values ucdVersion : Age_Values.values()) {
+            if (ucdVersion == Age_Values.Unassigned) {
+                continue;
+            }
+            IndexUnicodeProperties iup = IndexUnicodeProperties.make(ucdVersion);
+            UnicodeSet di = iup.load(prop).getSet(propValue);
+
+            final UnicodeMap<General_Category_Values> gc = iup.loadEnum(UcdProperty.General_Category,
+                    General_Category_Values.class);
+            UnicodeSet undefined = gc.getSet(General_Category_Values.Unassigned);
+
+            if (lastDi != null) {
+                if (!di.equals(lastDi)) {
+                    UnicodeSet removedDi = new UnicodeSet(lastDi).removeAll(di).freeze();
+                    UnicodeSet newDi = new UnicodeSet(di).removeAll(lastDi).retainAll(lastUndefined).freeze();
+                    UnicodeSet changedDi = new UnicodeSet(di).removeAll(lastDi).removeAll(lastUndefined).freeze();
+
+                    System.out.println(ucdVersion + "\t" + VersionToAge.ucd.getYear(ucdVersion) + "\tnew:\t"
+                            + newDi.size() + "\tREMOVED:\t" + removedDi.size() + "\tCHANGED:\t" + changedDi.size());
+                    show("new:", newDi);
+                    show("REMOVED:", removedDi);
+                    show("CHANGED:", changedDi);
+                }
+            }
+            lastDi = di;
+            lastUndefined = undefined;
+        }
+    }
+
+    private static void show(String string, UnicodeSet newDi) {
+        // abbreviate these classes of items.
+        newDi = removeCommon(string, newDi, "surrogates", surrogate);
+        newDi = removeCommon(string, newDi, "private-use", privateUse);
+        newDi = removeCommon(string, newDi, "nonchar-cp", nonchar);
+        newDi = removeCommon(string, newDi, "control", control);
+        newDi = removeCommon(string, newDi, "unassigned", unassigned);
+        for (String s : newDi) {
+            System.out.println(
+                    "\t\t" + string + "\t" + Utility.hex(s) + "\t" + birth.get(s) + "\t" + latest.getName(s, "+"));
+        }
+    }
+
+    private static UnicodeSet removeCommon(String string, UnicodeSet newDi, String nameOfSet, UnicodeSet unicodeSet) {
+        if (newDi.containsSome(unicodeSet)) {
+            UnicodeSet in = new UnicodeSet(newDi).retainAll(unicodeSet);
+            newDi = new UnicodeSet(newDi).removeAll(unicodeSet);
+            System.out.println("\t\t" + string + "\t" + in.size() + "\t" + nameOfSet + "\t" + in);
+        }
+        return newDi;
+    }
+}
