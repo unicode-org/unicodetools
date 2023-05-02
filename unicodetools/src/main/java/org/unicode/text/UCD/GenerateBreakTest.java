b8d4e739f8687b8a29b38fe0c9b2ada52790b5cd
Robin Leroy <egg.robin.leroy@gmail.com>
2023-05-02 21:06:02 +0200

    formatting

--- unicodetools/src/main/java/org/unicode/text/UCD/GenerateBreakTest.java
+++ unicodetools/src/main/java/org/unicode/text/UCD/GenerateBreakTest.java
@@ -1688,23 +1688,24 @@ public abstract class GenerateBreakTest implements UCD_Types {
                         // Quotation mark examples from L2/23-063.  All spaces are U+0020 SPACE.
                         // Swedish.
                         "”Jo, når’n da ha gått ett stöck te, så kommer’n te e å, å i åa ä e ö.”\n"
-                        + "”Vasa”, sa’n.\n"
-                        + "”Å i åa ä e ö”, sa ja.",
+                                + "”Vasa”, sa’n.\n"
+                                + "”Å i åa ä e ö”, sa ja.",
                         "En gång undföll det honom dock, medan han släpade på det våta "
-                        + "höet: »Varför är höet redan torrt och inkört där borta på Solbacken, "
-                        + "och här hos oss är det vått?» — »Därför att de ha oftare sol än vi.»",
+                                + "höet: »Varför är höet redan torrt och inkört där borta på "
+                                + "Solbacken, och här hos oss är det vått?» — »Därför att de ha "
+                                + "oftare sol än vi.»",
                         // French.
                         "vous me heurtez, vous dites : « Excusez-moi, » et vous croyez que cela "
-                        + "suffit ?",
+                                + "suffit ?",
                         "j’ai dit : « Excusez-moi. » Il me semble donc que c’est assez.",
                         "Et vise au front mon père en criant : « Caramba ! »\u2028"
-                        + "Le coup passa si près, que le chapeau tomba\u2028"
-                        + "Et que le cheval fit un écart en arrière.\u2028"
-                        + "« Donne-lui tout de même à boire, » dit mon père.",
+                                + "Le coup passa si près, que le chapeau tomba\u2028"
+                                + "Et que le cheval fit un écart en arrière.\u2028"
+                                + "« Donne-lui tout de même à boire, » dit mon père.",
                         "« Je me suis vengé […]\u2029"
-                        + "» On ne me verra ni parler ni écrire ; vous aurez eu mes dernières "
-                        + "paroles comme mes dernières adorations.\u2029"
-                        + "» J. S. »",
+                                + "» On ne me verra ni parler ni écrire ; vous aurez eu mes "
+                                + "dernières paroles comme mes dernières adorations.\u2029"
+                                + "» J. S. »",
                         // Vietnamese. Note that here we have a full stop *after* the quotation
                         // marks.
                         "— Không ai hãm bao giờ mà bây giờ hãm, thế nó mới « mới »."
