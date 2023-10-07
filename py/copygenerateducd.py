81e0e3708a2e26fe89b46b95b487e44eebc63e67
Robin Leroy <egg.robin.leroy@gmail.com>
2023-10-07 03:49:05 +0200

    Drop the traces

--- py/copygenerateducd.py
+++ py/copygenerateducd.py
@@ -18,8 +18,6 @@ import sys
 
 def main():
     out_of_source = '--out-of-source' in sys.argv[1:]
-    print(sys.argv,file=sys.stderr)
-    print(out_of_source,file=sys.stderr)
     cwd = Path().cwd()
     uversion = os.getenv("CURRENT_UVERSION")
     genucddir = (cwd / ".." if out_of_source else cwd) / "Generated" / "UCD" / uversion
