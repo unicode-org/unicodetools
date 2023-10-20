c4d0be95bb44b1e7070b192b27aead3c457762ee
Robin Leroy <egg.robin.leroy@gmail.com>
2023-10-20 02:09:16 +0200

    Do not break UnicodeData ranges

--- unicodetools/src/main/java/org/unicode/text/UCD/MakeUnicodeFiles.java
+++ unicodetools/src/main/java/org/unicode/text/UCD/MakeUnicodeFiles.java
@@ -662,6 +662,7 @@ public class MakeUnicodeFiles {
         final BagFormatter bf = new BagFormatter();
         bf.setHexValue(false)
                 .setMergeRanges(true)
+                .setRangeBreakSource(null)
                 .setNoSpacesBeforeSemicolon()
                 .setMinSpacesAfterSemicolon(0)
                 .setUnicodeDataStyleRanges(true)
