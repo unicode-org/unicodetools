9610a71a8e229134fefc983ef0a3a09c3cb5c0f6
Robin Leroy <egg.robin.leroy@gmail.com>
2023-12-15 12:22:25 +0100

    𒈬𒎌

--- c/uca/sifter/unisifeggs.cpp
+++ c/uca/sifter/unisifeggs.cpp
@@ -167,21 +167,21 @@ constexpr std::array<std::string_view, n> fields(std::string_view const line) {
     return result;
 }
 
-constexpr char32_t parseHexCodepoint(std::string_view hex) {
+constexpr char32_t parseHexCodePoint(std::string_view hex) {
     std::uint32_t c;
     std::from_chars(hex.data(), hex.data() + hex.size(), c, 16);
     return static_cast<char32_t>(c);
 }
 
-constexpr CodePointRange parseHexCodepointRange(std::string_view hex) {
+constexpr CodePointRange parseHexCodePointRange(std::string_view hex) {
     auto const first_dot = hex.find("..");
     if (first_dot == std::string_view::npos) {
-        char32_t const code_point = parseHexCodepoint(hex);
+        char32_t const code_point = parseHexCodePoint(hex);
         return CodePointRange::Inclusive(code_point, code_point);
     }
     return CodePointRange::Inclusive(
-            parseHexCodepoint(hex.substr(0, first_dot)),
-            parseHexCodepoint(hex.substr(first_dot + 2)));
+            parseHexCodePoint(hex.substr(0, first_dot)),
+            parseHexCodePoint(hex.substr(first_dot + 2)));
 }
 
 class UCD {
@@ -193,11 +193,11 @@ class UCD {
                                           .parent_path() /
                                   "unicodetools" / "data" / "ucd" / "dev";
         {
-            std::ifstream unicode_data(ucdDirectory / "UnicodeData.txt");
-            CHECK(unicode_data.good())
+            std::ifstream unicodeData(ucdDirectory / "UnicodeData.txt");
+            CHECK(unicodeData.good())
                     << "Run this tool from the c/uca/sifter directory";
             std::cout << "Reading UnicodeData.txt...\n";
-            for (std::string line; std::getline(unicode_data, line);) {
+            for (std::string line; std::getline(unicodeData, line);) {
                 // See https://www.unicode.org/reports/tr44/#UnicodeData.txt.
                 auto const [cp,
                             name,
@@ -214,21 +214,21 @@ class UCD {
                             suc,
                             slc,
                             stc] = fields<15>(line);
-                char32_t const codepoint = parseHexCodepoint(cp);
+                char32_t const codePoint = parseHexCodePoint(cp);
                 // TODO(egg): Handle ranges.
                 auto const range =
-                        CodePointRange::Inclusive(codepoint, codepoint);
+                        CodePointRange::Inclusive(codePoint, codePoint);
                 coarseGeneralCategory_[gc.front()].addAll(range);
                 generalCategory_[std::string(gc)].addAll(range);
-                canonicalCombiningClass_.emplace(codepoint,
+                canonicalCombiningClass_.emplace(codePoint,
                                                  std::stoi(std::string(ccc)));
                 if (!suc.empty()) {
-                    simpleUppercaseMapping_.emplace(codepoint,
-                                                    parseHexCodepoint(suc));
+                    simpleUppercaseMapping_.emplace(codePoint,
+                                                    parseHexCodePoint(suc));
                 }
                 if (!slc.empty()) {
-                    simpleLowercaseMapping_.emplace(codepoint,
-                                                    parseHexCodepoint(slc));
+                    simpleLowercaseMapping_.emplace(codePoint,
+                                                    parseHexCodePoint(slc));
                 }
             }
         }
@@ -246,13 +246,13 @@ class UCD {
                     continue;
                 }
                 auto const [cp, decimalNV, _, rationalNV] = fields<4>(record);
-                auto const range = parseHexCodepointRange(cp);
+                auto const range = parseHexCodePointRange(cp);
                 codePointsWithNumericValue_.addAll(range);
                 if (rationalNV.find_first_not_of("0123456789") ==
                     std::string_view::npos) {
-                    for (char32_t const codepoint : range) {
+                    for (char32_t const codePoint : range) {
                         naturalNumericValue_.emplace(
-                                codepoint, std::stoll(std::string(rationalNV)));
+                                codePoint, std::stoll(std::string(rationalNV)));
                     }
                 }
             }
@@ -329,7 +329,7 @@ class UCD {
             switch (std::count(record.begin(), record.end(), ';')) {
                 case 1: {
                     auto const [cp, propertyName] = fields<2>(record);
-                    CodePointRange const range = parseHexCodepointRange(cp);
+                    CodePointRange const range = parseHexCodePointRange(cp);
                     binaryProperties_[std::string(propertyName)].addAll(range);
                     break;
                 }
@@ -357,10 +357,6 @@ class UCD {
     CodePointSet codePointsWithNumericValue_;
 };
 
-typedef unsigned short UShort16;
-
-typedef unsigned int UInt32;
-
 UCD* const ucd = new UCD();
 
 static bool isSifterNonSpacingCombining(char32_t c) {
@@ -379,7 +375,7 @@ static bool isSpecialCJKIdeograph(char32_t c) {
 
 }  // namespace
 
-extern "C" int unisift_IsAlphabetic(UInt32 c) {
+extern "C" int unisift_IsAlphabetic(std::uint32_t c) {
     return ucd->binaryPropertySet("Alphabetic").contains(c) &&
            // HACK: TODO: Deal with the 12 special CJK ideographs
            // ("compatibility" but no decompositions) in sift() before testing
@@ -391,46 +387,46 @@ extern "C" int unisift_IsAlphabetic(UInt32 c) {
            !(c == 0x1E13C || c == 0x1E13D || c == 0x16B42 || c == 0x16B43);
 }
 
-extern "C" int unisift_IsNonSpacing(UInt32 c) {
+extern "C" int unisift_IsNonSpacing(std::uint32_t c) {
     return ucd->generalCategorySet("Mn").contains(c) ||
            isSifterNonSpacingCombining(c);
 }
-extern "C" int unisift_IsCombining(UInt32 c) {
+extern "C" int unisift_IsCombining(std::uint32_t c) {
     return ucd->generalCategorySet("M").contains(c) ||
            isSifterNonSpacingCombining(c);
 }
-extern "C" int unisift_IsExtender(UInt32 c) {
+extern "C" int unisift_IsExtender(std::uint32_t c) {
     return ucd->binaryPropertySet("Extender").contains(c);
 }
-extern "C" int unisift_IsCurrency(UInt32 c) {
+extern "C" int unisift_IsCurrency(std::uint32_t c) {
     return ucd->generalCategorySet("Sc").contains(c);
 }
-extern "C" int unisift_IsPunctuation(UInt32 c) {
+extern "C" int unisift_IsPunctuation(std::uint32_t c) {
     return ucd->generalCategorySet("P").contains(c);
 }
-extern "C" int unisift_IsDiacritic(UInt32 c) {
+extern "C" int unisift_IsDiacritic(std::uint32_t c) {
     return ucd->binaryPropertySet("Diacritic").contains(c);
 }
-extern "C" int unisift_IsNumeric(UInt32 c) {
+extern "C" int unisift_IsNumeric(std::uint32_t c) {
     return ucd->generalCategorySet("N").contains(c);
 }
-extern "C" int unisift_IsDecimalDigit(UInt32 c) {
+extern "C" int unisift_IsDecimalDigit(std::uint32_t c) {
     return ucd->generalCategorySet("Nd").contains(c);
 }
-extern "C" int unisift_IsWhiteSpace(UInt32 c) {
+extern "C" int unisift_IsWhiteSpace(std::uint32_t c) {
     return ucd->binaryPropertySet("White_Space").contains(c);
 }
-extern "C" int unisift_IsMath(UInt32 c) {
+extern "C" int unisift_IsMath(std::uint32_t c) {
     return ucd->binaryPropertySet("Math").contains(c);
 }
-extern "C" int unisift_IsIdeographic(UInt32 c) {
+extern "C" int unisift_IsIdeographic(std::uint32_t c) {
     return ucd->binaryPropertySet("Ideographic").contains(c);
 }
-extern "C" int unisift_IsMiscSymbolic(UInt32 c) {
+extern "C" int unisift_IsMiscSymbolic(std::uint32_t c) {
     return ucd->generalCategorySet("Sk").contains(c) ||
            ucd->generalCategorySet("So").contains(c);
 }
-extern "C" int unisift_IsIgnorable(UInt32 c) {
+extern "C" int unisift_IsIgnorable(std::uint32_t c) {
     return ((ucd->generalCategorySet("Cc").contains(c) ||
              ucd->generalCategorySet("Cf").contains(c)) &&
             !ucd->binaryPropertySet("White_Space").contains(c)) ||
@@ -438,24 +434,24 @@ extern "C" int unisift_IsIgnorable(UInt32 c) {
            c == U'\u00AD';
 }
 
-extern "C" int unisift_IsUpper(UInt32 c) {
+extern "C" int unisift_IsUpper(std::uint32_t c) {
     return ucd->binaryPropertySet("Uppercase").contains(c);
 }
 
-extern "C" UInt32 unisift_ToLower(UInt32 c) {
+extern "C" std::uint32_t unisift_ToLower(std::uint32_t c) {
     return ucd->simpleLowercaseMapping(c);
 }
 
-extern "C" UInt32 unisift_ToUpper(UInt32 c) {
+extern "C" std::uint32_t unisift_ToUpper(std::uint32_t c) {
     return ucd->simpleUppercaseMapping(c);
 }
 
-extern "C" int unisift_ToIntValue(UInt32 c) {
+extern "C" int unisift_ToIntValue(std::uint32_t c) {
     // TODO(egg): The conversion to the return type is UB for large numbers.
     return ucd->naturalNumericValue(c).value_or(
             ucd->codePointsWithNumericValue().contains(c) ? -2 : -1);
 }
 
-extern "C" int unisift_GetCombiningClass(UInt32 c) {
+extern "C" int unisift_GetCombiningClass(std::uint32_t c) {
     return ucd->canonicalCombiningClass(c);
 }
