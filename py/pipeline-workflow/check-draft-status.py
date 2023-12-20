d8007f28917d39e9a6eaecd950ef67fc39b85278
Robin Leroy <egg.robin.leroy@gmail.com>
2023-12-20 15:35:06 +0100

    eol@eof, .0

--- py/pipeline-workflow/check-draft-status.py
+++ py/pipeline-workflow/check-draft-status.py
@@ -5,7 +5,7 @@ import json
 pipeline_label = os.environ['PIPELINE_LABEL']
 
 with open("unicodetools/data/ucd/dev/DerivedAge.txt", 'r') as f:
-    version = f.readline().strip().replace("# DerivedAge-", "").replace(".txt", "")
+    version = f.readline().strip().replace("# DerivedAge-", "").replace(".0.txt", "")
 
 if pipeline_label != "pipeline-" + version:
     with open(os.environ['GITHUB_EVENT_PATH'], 'r') as f:
