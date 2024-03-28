a6d1fb53d14cc25eaee818d54b0ba74b53a2f979
Robin Leroy <egg.robin.leroy@gmail.com>
2024-03-28 02:17:52 +0100

    More lenient check for UTC decision links, and check for SAH issues

--- py/pipeline-workflow/check-utc-decision.py
+++ py/pipeline-workflow/check-utc-decision.py
@@ -7,7 +7,7 @@ with open(os.environ['GITHUB_EVENT_PATH'], 'r') as f:
     event = json.load(f)
 print(event)
 pr_body = event['pull_request']['body']
-if not re.search(r"UTC-\d\d\d-[MC]\d", pr_body):
+if not re.search(r"(UTC-|(unicode\.org/(L2/L\d+/\d+\.htm#|cgi-bin/GetL2Ref\.pl\?))\d\d\d-[MC]\d", pr_body):
     print("::error title=Need UTC decision::"
           "PRs for approved or provisionally assigned must include a link to a "
           "UTC decision.")
