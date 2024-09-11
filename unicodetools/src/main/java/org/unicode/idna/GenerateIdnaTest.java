b441fe533098b92fd4f7e23394a930f0cc89e43c
Robin Leroy <egg.robin.leroy@gmail.com>
2024-09-12 01:36:06 +0200

    Manually fix a dozen files

--- unicodetools/src/main/java/org/unicode/idna/GenerateIdnaTest.java
+++ unicodetools/src/main/java/org/unicode/idna/GenerateIdnaTest.java
@@ -176,7 +176,7 @@ public class GenerateIdnaTest {
             // See https://github.com/unicode-org/unicodetools/issues/100 "Whither 13.1.0?"
             lastVersion = "13.0.0";
         }
-        int ucdTypesLastVersion = UCD_Types.AGE151;
+        int ucdTypesLastVersion = UCD_Types.AGE160;  // FIX_FOR_NEW_VERSION
         String ucdTypesLastVersionString = UCD_Types.AGE_VERSIONS[ucdTypesLastVersion];
         if (!ucdTypesLastVersionString.equals(lastVersion)) {
             throw new AssertionError(
