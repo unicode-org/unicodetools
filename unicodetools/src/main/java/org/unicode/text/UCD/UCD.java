46bb6d231b906e06c8ab287a5a8f3d142c88194d
Robin Leroy <eggrobin@unicode.org>
2024-11-15 17:10:15 +0100

    Two Tangut ideographs (#951)
    * UnicodeData.txt and hardcoded ranges from L2/23-246
    
    * LineBreak.txt
    
    * new block
    
    * Regenerate UCD
    
    * GenerateEnums
    
    * EAW=W
    
    * Regenerate UCD
    
    * SUP
    
    * script
    
    * Regenerate UCD
    
    * Do not break UnicodeData ranges
    
    * Regenerate UCD
    
    * sources
    
    * Tangut Components Supplement to vo=U
    
    * Regenerate UCD
    
    * Ideographic
    
    * Regenerate UCD
    
    * Bump the upper bound of the Tangut Supplement
    
    * Regenerate UCD
    
    * TangutSources.txt lines from the proposal
    
    * Document the source
    
    * A test
    
    * The hobgoblin of little minds

--- unicodetools/src/main/java/org/unicode/text/UCD/UCD.java
+++ unicodetools/src/main/java/org/unicode/text/UCD/UCD.java
@@ -1309,9 +1309,8 @@ public final class UCD implements UCD_Types {
                     // Unicode 12 added TANGUT IDEOGRAPH-187F2..TANGUT IDEOGRAPH-187F7.
                     return TANGUT_BASE;
                 }
-                // REMOVE BEFORE FLIGHT: The actual version will be greater than 0x100000.
-                if (ch <= 0x187FF && rCompositeVersion >= 0x100000) {
-                    // Unicode [..] added TANGUT IDEOGRAPH-187F8..TANGUT IDEOGRAPH-187FF.
+                if (ch <= 0x187FF && rCompositeVersion >= 0x110000) {
+                    // Unicode 17 added TANGUT IDEOGRAPH-187F8..TANGUT IDEOGRAPH-187FF.
                     return TANGUT_BASE;
                 }
             }
@@ -1326,9 +1325,12 @@ public final class UCD implements UCD_Types {
                 if (ch <= 0x18D08) {
                     return TANGUT_SUP_BASE; // 18D00..18D08 Tangut Ideograph Supplement
                 }
-                // REMOVE BEFORE FLIGHT: The actual version will be greater than 0x100000.
-                if (ch <= 0x18D1C && rCompositeVersion >= 0x100000) {
-                    // Unicode [..] added TANGUT IDEOGRAPH-18D09..TANGUT IDEOGRAPH-18D1C.
+                if (ch <= 0x18D1C && rCompositeVersion >= 0x110000) {
+                    // Unicode 17 added TANGUT IDEOGRAPH-18D09..TANGUT IDEOGRAPH-18D1C.
+                    return TANGUT_SUP_BASE;
+                }
+                if (ch <= 0x18D1E && rCompositeVersion >= 0x110000) {
+                    // Unicode 17 added TANGUT IDEOGRAPH-18D1D..TANGUT IDEOGRAPH-18D1E.
                     return TANGUT_SUP_BASE;
                 }
             }
