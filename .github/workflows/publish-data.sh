03710abf32bd42a97f8ea15fddaee4ba4feabc29
Liang Hai <lianghai@gmail.com>
2024-12-12 17:31:22 +0100

    Workflow renaming, mode → release phase,  dist → TMP=pub/tmp

--- .github/workflows/publish-data.sh
+++ .github/workflows/publish-data.sh
@@ -1,10 +1,11 @@
-# See publish-ucd.yml
+# See publish-data.yml
 
-mkdir dist
+TMP=pub/tmp
+mkdir $TMP
 
 PUB_DATE=$(date --iso-8601)
 
-cat > dist/sed-readmes.txt << eof
+cat > $TMP/sed-readmes.txt << eof
 s/COPY_YEAR/$COPY_YEAR/
 s/PUB_DATE/$PUB_DATE/
 s/PUB_STATUS/draft/
@@ -15,43 +16,43 @@ s%PUBLIC_EMOJI%Public/draft/emoji%
 s%PUBLIC_UCD%Public/draft/UCD%
 eof
 
-mkdir dist/UCD
-cp -R unicodetools/data/ucd/dev dist/UCD/ucd
-mv dist/UCD/ucd/version-ReadMe.txt dist/UCD/ReadMe.txt
-rm -r dist/UCD/ucd/Unihan
+mkdir $TMP/UCD
+cp -R unicodetools/data/ucd/dev $TMP/UCD/ucd
+mv $TMP/UCD/ucd/version-ReadMe.txt $TMP/UCD/ReadMe.txt
+rm -r $TMP/UCD/ucd/Unihan
 
-if [ "$MODE" = "Snapshot" ]; then
-    rm -r dist/UCD/ucd/emoji
+if [ "$RELEASE_PHASE" = "Dev" ]; then
+    rm -r $TMP/UCD/ucd/emoji
 fi
 
-if [ "$MODE" = "Alpha" ] || [ "$MODE" = "Beta" ]; then
-    cp -R unicodetools/data/emoji/dev dist/emoji
+if [ "$RELEASE_PHASE" = "Alpha" ] || [ "$RELEASE_PHASE" = "Beta" ]; then
+    cp -R unicodetools/data/emoji/dev $TMP/emoji
 
-    cp -R unicodetools/data/idna/dev dist/idna
+    cp -R unicodetools/data/idna/dev $TMP/idna
 
-    mkdir dist/idna2008derived
-    cp unicodetools/data/idna/idna2008derived/ReadMe.txt dist/idna2008derived
-    cp unicodetools/data/idna/idna2008derived/Idna2008-$UNI_VER.txt dist/idna2008derived
+    mkdir $TMP/idna2008derived
+    cp unicodetools/data/idna/idna2008derived/ReadMe.txt $TMP/idna2008derived
+    cp unicodetools/data/idna/idna2008derived/Idna2008-$UNI_VER.txt $TMP/idna2008derived
 fi
 
-if [ "$MODE" = "Beta" ]; then
-    cp -R unicodetools/data/uca/dev dist/UCA
-    sed -i -f dist/sed-readmes.txt dist/UCA/CollationTest.html
+if [ "$RELEASE_PHASE" = "Beta" ]; then
+    cp -R unicodetools/data/uca/dev $TMP/UCA
+    sed -i -f $TMP/sed-readmes.txt $TMP/UCA/CollationTest.html
 
-    cp -R unicodetools/data/security/dev dist/security
+    cp -R unicodetools/data/security/dev $TMP/security
 fi
 
 # Update the readmes in-place (-i) as set up above.
-find dist -name '*ReadMe.txt' | xargs sed -i -f dist/sed-readmes.txt
-rm dist/sed-readmes.txt
+find $TMP -name '*ReadMe.txt' | xargs sed -i -f $TMP/sed-readmes.txt
+rm $TMP/sed-readmes.txt
 
-mkdir dist/zipped
-mv dist/UCD/ucd/zipped-ReadMe.txt dist/zipped/ReadMe.txt
-(cd dist/UCD/ucd; zip -r UCD.zip *)
-mv dist/UCD/ucd/UCD.zip dist/zipped
+mkdir $TMP/zipped
+mv $TMP/UCD/ucd/zipped-ReadMe.txt $TMP/zipped/ReadMe.txt
+(cd $TMP/UCD/ucd; zip -r UCD.zip *)
+mv $TMP/UCD/ucd/UCD.zip $TMP/zipped
 
-if [ "$MODE" = "Beta" ]; then
-    (cd dist/UCA; zip -r CollationTest.zip CollationTest; rm -r CollationTest)
+if [ "$RELEASE_PHASE" = "Beta" ]; then
+    (cd $TMP/UCA; zip -r CollationTest.zip CollationTest; rm -r CollationTest)
 
-    (cd dist/security; zip -r uts39-data-$UNI_VER.zip *)
+    (cd $TMP/security; zip -r uts39-data-$UNI_VER.zip *)
 fi
