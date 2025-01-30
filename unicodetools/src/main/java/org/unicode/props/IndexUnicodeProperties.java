3c1ec5ecef9a16db754429faa90238b5c0d1727a
Robin Leroy <egg.robin.leroy@gmail.com>
2025-01-30 02:17:55 +0100

    Somehow the code in UCD.java becomes a little bit cleaner

--- unicodetools/src/main/java/org/unicode/props/IndexUnicodeProperties.java
+++ unicodetools/src/main/java/org/unicode/props/IndexUnicodeProperties.java
@@ -28,6 +28,7 @@ import java.util.Collections;
 import java.util.EnumMap;
 import java.util.EnumSet;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Locale;
@@ -839,9 +840,19 @@ public class IndexUnicodeProperties extends UnicodeProperty.Factory {
         }
 
         @Override
-        protected List<String> _getAvailableValues(List result) {
+        protected List<String> _getAvailableValues(List<String> result) {
             if (stringToNamedEnum != null) {
                 result.addAll(enumValueNames);
+                return result;
+            }
+            if (isMultivalued()) {
+                HashSet<String> valueSet = new HashSet<>();
+                for (var value : _getUnicodeMap().getAvailableValues()) {
+                    for (var part : delimiterSplitter.split(value)) {
+                        valueSet.add(part);
+                    }
+                }
+                result.addAll(valueSet);
                 return result;
             }
             return _getUnicodeMap().getAvailableValues(result);
