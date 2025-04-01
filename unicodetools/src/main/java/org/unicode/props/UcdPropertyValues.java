be568bf4f0c70a5447f770bc8da7d3329654b837
Robin Leroy <egg.robin.leroy@gmail.com>
2025-04-01 17:25:38 +0200

    GenerateEnums

--- unicodetools/src/main/java/org/unicode/props/UcdPropertyValues.java
+++ unicodetools/src/main/java/org/unicode/props/UcdPropertyValues.java
@@ -982,8 +982,8 @@ public class UcdPropertyValues {
     }
 
     public enum Idn_2008_Values implements Named {
-        NV8("nv8"),
-        XV8("xv8"),
+        nv8("nv8"),
+        xv8("xv8"),
         na("na");
         private final PropertyNames<Idn_2008_Values> names;
 
