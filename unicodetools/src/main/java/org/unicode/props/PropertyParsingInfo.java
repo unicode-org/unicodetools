c4e8facc97cc22f87539db3aa10d291d28eedbfa
Robin Leroy <egg.robin.leroy@gmail.com>
2025-04-07 11:12:36 +0200

    Also log the property name

--- unicodetools/src/main/java/org/unicode/props/PropertyParsingInfo.java
+++ unicodetools/src/main/java/org/unicode/props/PropertyParsingInfo.java
@@ -409,7 +409,7 @@ public class PropertyParsingInfo implements Comparable<PropertyParsingInfo> {
     public String checkEnum(String string) {
         final Enum item = string == null ? null : property.getEnum(string);
         if (item == null) {
-            throw new UnicodePropertyException("\tBad enum value:\t" + string);
+            throw new UnicodePropertyException("\tBad enum value for " + property + " :\t" + string);
         } else {
             string = item.toString();
         }
