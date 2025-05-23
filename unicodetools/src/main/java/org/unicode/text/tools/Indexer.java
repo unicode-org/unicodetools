91c41cf7539e7142e72767ad12efb84495141292
Robin Leroy <egg.robin.leroy@gmail.com>
2025-05-23 03:16:06 +0200

    meow

--- unicodetools/src/main/java/org/unicode/text/tools/Indexer.java
+++ unicodetools/src/main/java/org/unicode/text/tools/Indexer.java
@@ -6,7 +6,9 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.io.StringWriter;
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
@@ -15,7 +17,9 @@ import org.unicode.props.IndexUnicodeProperties;
 import org.unicode.props.UcdProperty;
 import org.unicode.props.UnicodeProperty;
 import org.unicode.text.utility.Utility;
+import org.unicode.tools.Segmenter;
 
+import com.ibm.icu.text.BreakIterator;
 import com.ibm.icu.text.Collator;
 import com.ibm.icu.text.Transliterator;
 import com.ibm.icu.text.UnicodeSet;
@@ -80,29 +84,65 @@ public class Indexer {
       final var informalAlias = iup.getProperty(UcdProperty.Names_List_Alias);
       final var block = iup.getProperty(UcdProperty.Block);
       final var subheader = iup.getProperty(UcdProperty.Names_List_Subheader);
+      final var subheader_notice = iup.getProperty(UcdProperty.Names_List_Subheader_Notice);
       final var comment = iup.getProperty(UcdProperty.Names_List_Comment);
       for (int cp = 0; cp <= 0x10FFFF; ++cp) {
-        final String characterName = name.getValue(cp);
-        if (characterName != null && !characterName.contains(Utility.hex(cp))) {
+        String characterName = name.getValue(cp);
+        if (characterName != null) {
+          characterName = characterName.replace(Utility.hex(cp), "#");
           index.computeIfAbsent(new IndexKey(characterName, name), k -> new UnicodeSet()).add(cp);
         }
-        for (var prop : new UnicodeProperty[] { nameAlias, informalAlias, block, subheader, comment }) {
+        for (var prop : new UnicodeProperty[] { nameAlias, informalAlias, block, subheader, comment, subheader_notice }) {
           for (String key : prop.getValues(cp)) {
             if (key == null) {
               continue;
             }
+            if (prop.getName().equals("Block")) {
+              key = key.replace("_", " ");
+            }
             index.computeIfAbsent(new IndexKey(key, prop), k -> new UnicodeSet()).add(cp);
           }
+        }
+        if (cp % 0x10000 == 0xFFFF) {
+          System.err.println("Leaves for plane " + cp / 0x10000);
         }
       }
       var file = new PrintStream(new File("index.html"));
       file.println("<table>");
+      Map<String, UnicodeSet> wordIndex = new TreeMap<>(Collator.getInstance());
+      final var wordBreak = BreakIterator.getWordInstance();
       for (var entry : index.entrySet()) {
-        System.out.println(entry.getKey().key);
+        //System.out.println(entry.getKey().key);
         file.println("<tr><td>" + toHTML.transform(entry.getKey().key) + "</td><td>" + htmlIndexValue(entry.getValue())+"</tr>");
+        wordBreak.setText(entry.getKey().key);
+        int start = 0;
+        for (int end = wordBreak.next();
+             end != BreakIterator.DONE;
+             start = end, end = wordBreak.next()) {
+          if (wordBreak.getRuleStatus() >= BreakIterator.WORD_LETTER) {
+            wordIndex.computeIfAbsent(entry.getKey().key.substring(start, end),
+            k-> new UnicodeSet()).addAll(entry.getValue());
+          }
+        }
+      }
+      System.err.println("Exctracted words");
+      for (var entry : wordIndex.entrySet().stream().sorted((left, right) -> Integer.compare(left.getValue().size(), right.getValue().size())).toList()) {
+        System.err.println(entry.getKey() + " " + entry.getValue().size() + " characters in " + blockCount(entry.getValue()) + " blocks");
       }
       file.println("</table>");
       file.close();
+    }
+
+    static int blockCount(UnicodeSet characters) {
+      final var iup = IndexUnicodeProperties.make();
+      final var block = iup.getProperty(UcdProperty.Block);
+      UnicodeSet remainder = characters.cloneAsThawed();
+      int count = 0;
+      while (!remainder.isEmpty()) {
+        ++count;
+        remainder.removeAll(block.getSet(block.getValue(remainder.charAt(0))));
+      }
+      return count;
     }
 
     public static String htmlIndexValue(UnicodeSet characters) {
