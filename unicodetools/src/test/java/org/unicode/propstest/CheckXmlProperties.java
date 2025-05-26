e6e34684d8635e1360eddbda22f1350896f2e939
John Wilcock <jowilco@microsoft.com>
2025-05-26 10:27:19 -0700

    CheckXmlProperties and Grouped NoUnihan fix. (#1156)
    * Fixed an issue with grouped noUnihan not showing nonCJK properties for Unihan range. Updated CheckXmlProperties to work with UCDXML for 17.0

--- unicodetools/src/test/java/org/unicode/propstest/CheckXmlProperties.java
+++ unicodetools/src/test/java/org/unicode/propstest/CheckXmlProperties.java
@@ -2,6 +2,9 @@ package org.unicode.propstest;
 
 import com.ibm.icu.impl.UnicodeMap;
 import com.ibm.icu.text.UnicodeSet;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import org.unicode.cldr.util.Timer;
 import org.unicode.props.IndexUnicodeProperties;
 import org.unicode.props.UcdProperty;
@@ -108,16 +111,79 @@ public class CheckXmlProperties {
                     // just for debugging
                     final String xx = XMLProperties.getXmlResolved(prop, i, xmap.get(i));
                     final String ii = iup.getResolvedValue(prop, i);
-
-                    if ((xval == null || xval.isEmpty()) && (upval == null || upval.isEmpty())) {
-                        empty.add(i);
-                    } else {
-                        errorMap.put(
-                                i,
-                                " codepoints where up=\t"
-                                        + XMLProperties.show(upval)
-                                        + "\t& xml=\t"
-                                        + XMLProperties.show(xval));
+                    // UCDXML only contains cjkNumeric values when they are not null/NaN.
+                    switch (prop) {
+                        case kAccountingNumeric:
+                        case kOtherNumeric:
+                        case kPrimaryNumeric:
+                            if (upval != null && upval.equals("NaN") && xval == null) {
+                                empty.add(i);
+                            } else {
+                                errorMap.put(
+                                        i,
+                                        " codepoints where up=\t"
+                                                + XMLProperties.show(upval)
+                                                + "\t& xml=\t"
+                                                + XMLProperties.show(xval));
+                            }
+                            break;
+                        case Name_Alias:
+                            // Name_Alias is sorted for display in UCDXML.
+                            List<String> xList = Arrays.asList(xval.split("; "));
+                            List<String> upList =
+                                    (ii != null ? Arrays.asList(ii.split("\\|")) : null);
+                            // Collections.sort(xList);
+                            Collections.sort(upList);
+                            if (!xList.equals(upList)) {
+                                errorMap.put(
+                                        i,
+                                        " codepoints where up=\t"
+                                                + XMLProperties.show(upval)
+                                                + "\t& xml=\t"
+                                                + XMLProperties.show(xval));
+                            }
+                            break;
+                        case kEH_Core:
+                            if (upval != null && upval.equals("None") && xval == null) {
+                                empty.add(i);
+                            } else {
+                                errorMap.put(
+                                        i,
+                                        " codepoints where up=\t"
+                                                + XMLProperties.show(upval)
+                                                + "\t& xml=\t"
+                                                + XMLProperties.show(xval));
+                            }
+                            break;
+                        case kEH_NoMirror:
+                        case kEH_NoRotate:
+                            if (upval != null && upval.equals("No") && xval == null) {
+                                empty.add(i);
+                            } else {
+                                errorMap.put(
+                                        i,
+                                        " codepoints where up=\t"
+                                                + XMLProperties.show(upval)
+                                                + "\t& xml=\t"
+                                                + XMLProperties.show(xval));
+                            }
+                            break;
+                        default:
+                            if ((xval == null
+                                            || xval.isEmpty()
+                                            || xval.equals(Character.toString(i)))
+                                    && (upval == null
+                                            || upval.isEmpty()
+                                            || upval.equals(Character.toString(i)))) {
+                                empty.add(i);
+                            } else {
+                                errorMap.put(
+                                        i,
+                                        " codepoints where up=\t"
+                                                + XMLProperties.show(upval)
+                                                + "\t& xml=\t"
+                                                + XMLProperties.show(xval));
+                            }
                     }
                 }
             }
@@ -138,7 +204,7 @@ public class CheckXmlProperties {
                     int showCount = 0;
                     for (String value : errorMap.values()) {
                         System.out.println(
-                                "\t" + value + "\t: " + errorMap.getSet(value).toPattern(false));
+                                "\t" + value + "\t: " + errorMap.getSet(value).toString());
                         if (++showCount > MAX_SHOW) {
                             System.out.println("… " + (errorMap.size() - MAX_SHOW) + " more");
                             break;
