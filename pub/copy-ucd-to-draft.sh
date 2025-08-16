8d421378ae70bb8eee311e7dd07e5ff1b2d8d222
Markus Scherer <markus.icu@gmail.com>
2025-08-15 19:37:39 -0700

    pub final uni17 & align pub scripts

--- pub/copy-ucd-to-draft.sh
+++ pub/copy-ucd-to-draft.sh
@@ -6,7 +6,7 @@
 # pub/copy-ucd-to-draft.sh  ~/unitools/mine/src  /tmp/unicode/Public/draft
 
 UNICODETOOLS=$1
-DRAFT=$2
+DEST=$2
 
 UNITOOLS_DATA=$UNICODETOOLS/unicodetools/data
 
@@ -17,9 +17,9 @@ EMOJI_VER=17.0
 
 TODAY=`date --iso-8601`
 
-mkdir -p $DRAFT
+mkdir -p $DEST
 
-cat > $DRAFT/sed-readmes.txt << eof
+cat > $DEST/sed-readmes.txt << eof
 s/COPY_YEAR/$COPY_YEAR/
 s/PUB_DATE/$TODAY/
 s/PUB_STATUS/draft/
@@ -29,29 +29,29 @@ s%PUBLIC_EMOJI%Public/draft/emoji%
 s%PUBLIC_UCD%Public/draft%
 eof
 
-mkdir -p $DRAFT/ucd
-cp -r $UNITOOLS_DATA/ucd/dev/* $DRAFT/ucd
-rm -r $DRAFT/ucd/Unihan
-rm -r $DRAFT/ucd/emoji
-mv $DRAFT/ucd/version-ReadMe.txt $DRAFT/ReadMe.txt
+mkdir -p $DEST/ucd
+cp -r $UNITOOLS_DATA/ucd/dev/* $DEST/ucd
+rm -r $DEST/ucd/Unihan
+rm -r $DEST/ucd/emoji
+mv $DEST/ucd/version-ReadMe.txt $DEST/ReadMe.txt
 
 # Fix permissions. Everyone can read, and search directories.
-chmod a+rX -R $DRAFT
+chmod a+rX -R $DEST
 
 # Update the readmes in-place (-i) as set up above.
-find $DRAFT -name '*ReadMe.txt' | xargs sed -i -f $DRAFT/sed-readmes.txt
+find $DEST -name '*ReadMe.txt' | xargs sed -i -f $DEST/sed-readmes.txt
 
 # Zip files for some types of data, after fixing permissions
-rm $DRAFT/ucd/UCD.zip
-(cd $DRAFT/ucd; zip -r UCD.zip *)
+rm $DEST/ucd/UCD.zip
+(cd $DEST/ucd; zip -r UCD.zip *)
 
 # Cleanup
-rm $DRAFT/sed-readmes.txt
+rm $DEST/sed-readmes.txt
 
-rm $DRAFT/ucd-snapshot.zip
-(cd $DRAFT; zip -r ucd-snapshot.zip *)
+rm $DEST/ucd-snapshot.zip
+(cd $DEST; zip -r ucd-snapshot.zip *)
 
 echo "--------------------"
 echo "Copy files from elsewhere:"
-echo "- Unihan.zip to $DRAFT/ucd"
+echo "- Unihan.zip to $DEST/ucd"
 
