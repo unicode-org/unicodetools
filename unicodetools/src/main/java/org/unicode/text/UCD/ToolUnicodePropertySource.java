615fa9fdcaa72ba508b03c0dad3433afdc6b47f5
Robin Leroy <egg.robin.leroy@gmail.com>
2025-11-10 14:34:35 +0100

    Regenerate P(V)A

--- unicodetools/src/main/java/org/unicode/text/UCD/ToolUnicodePropertySource.java
+++ unicodetools/src/main/java/org/unicode/text/UCD/ToolUnicodePropertySource.java
@@ -319,6 +319,10 @@ public class ToolUnicodePropertySource extends UnicodeProperty.Factory {
         add(iup.getProperty("kEH_JSesh"));
         add(iup.getProperty("kEH_NoMirror"));
         add(iup.getProperty("kEH_NoRotate"));
+        add(iup.getProperty("kSEAL_THXSrc"));
+        add(iup.getProperty("kSEAL_CCZSrc"));
+        add(iup.getProperty("kSEAL_DYCSrc"));
+        add(iup.getProperty("kSEAL_QJZSrc"));
         add(iup.getProperty("Emoji"));
         add(iup.getProperty("Emoji_Presentation"));
         add(iup.getProperty("Emoji_Modifier"));
