458cc3f492f3a20782874cee9935060058318501
Markus Scherer <markus.icu@gmail.com>
2026-02-04 08:04:50 -0800

    update pub scripts for 18

--- pub/copy-final.sh
+++ pub/copy-final.sh
@@ -11,7 +11,7 @@ DEST=$2
 UNITOOLS_DATA=$UNICODETOOLS/unicodetools/data
 
 # Adjust the following for each year and version as needed.
-COPY_YEAR=2025
+COPY_YEAR=2026
 UNI_VER=18.0.0
 EMOJI_VER=18.0
 
@@ -45,6 +45,9 @@ cp $UNITOOLS_DATA/idna/dev/* $DEST/idna
 
 mkdir -p $DEST/security
 cp $UNITOOLS_DATA/security/dev/* $DEST/security
+
+mkdir -p $DEST/linkification
+cp $UNITOOLS_DATA/linkification/dev/* $DEST/linkification
 
 # Fix permissions. Everyone can read, and search directories.
 chmod a+rX -R $DEST
