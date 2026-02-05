5a1544922e9f3092578e1b338ccac35efa831604
John Wilcock <jowilco@microsoft.com>
2026-02-05 12:42:25 -0800

    Revert the revert

--- unicodetools/src/main/java/org/unicode/text/UCD/MakeUnicodeFiles.java
+++ unicodetools/src/main/java/org/unicode/text/UCD/MakeUnicodeFiles.java
@@ -69,8 +69,11 @@ public class MakeUnicodeFiles {
     public static void main(String[] args) throws IOException {
 
         // Copy generated files to the dev directory by default
-        boolean cleanAndCopy =
-                Arrays.asList(args).contains("-c"); // clean Bin & copy changed output
+        boolean cleanAndCopy = true;
+        if (Arrays.asList(args).contains("--no-copy")) {
+            // Leave dev untouched, files are only in Generated directory
+            cleanAndCopy = false;
+        }
 
         int files = Arrays.asList(args).indexOf("--generate");
         if (files >= 0) {
