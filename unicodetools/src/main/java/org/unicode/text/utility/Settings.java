8ff9cda3331f454cc054cb25f55990c04cf4c7d3
Robin Leroy <egg.robin.leroy@gmail.com>
2026-09-21 21:21:43 +0200

    Settings.java

--- unicodetools/src/main/java/org/unicode/text/utility/Settings.java
+++ unicodetools/src/main/java/org/unicode/text/utility/Settings.java
@@ -22,7 +22,7 @@ public class Settings {
     // Are they equivalent for our purposes?
 
     /** Used for the default version. */
-    public static final String latestVersion = "18.0.0";
+    public static final String latestVersion = "19.0.0";
 
     public enum ReleasePhase {
         DEV("dev"), // Before α.
@@ -44,9 +44,9 @@ public class Settings {
         }
     };
 
-    public static final ReleasePhase latestVersionPhase = ReleasePhase.GAMMA;
+    public static final ReleasePhase latestVersionPhase = ReleasePhase.DEV;
 
-    public static final String lastVersion = "17.0.0"; // last released version
+    public static final String lastVersion = "18.0.0"; // last released version
 
     public static final VersionInfo LATEST_VERSION_INFO = VersionInfo.getInstance(latestVersion);
     public static final VersionInfo LAST_VERSION_INFO = VersionInfo.getInstance(lastVersion);
